d73c1d434e3f4edf2488ae8408abf64d76bbd462
Hazman Hatadi <hatadihazman@gmail.com>
2022-03-21 16:19:02 +0700

    logo

--- frontend/header.php
+++ frontend/header.php
@@ -131,13 +131,13 @@
 									while($d=mysqli_fetch_array($sql)){$x=$d['cat_id'];
 										?>
         						<!-- DROPDOWN MENU -->
-					        	<li aria-haspopup="true"><a href="#"><?php echo $d['cat_name'];?><span class="wsarrow"></span></a>
+					        	<li aria-haspopup="true"><a href="<?php echo $d['logo']; ?>"><?php echo $d['cat_name'];?><span class="wsarrow"></span></a>
 					           		<ul class="sub-menu">
 									   <?php
 									$sql2 = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM navbar where cat_parent='$x' and cat_desc=2");
 									while($d2=mysqli_fetch_array($sql2)){
 										?>
-					           			<li aria-haspopup="true"><a href="#"><?php echo $d2['cat_name'];?></a></li>
+					           			<li aria-haspopup="true"><a href="<?php echo $d2['logo']; ?>"><?php echo $d2['cat_name'];?></a></li>
 					              		<?php } ?>
 					           		</ul>
 					          	</li>	<!-- END DROPDOWN MENU -->
