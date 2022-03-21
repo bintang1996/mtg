270a11f9fce68f6e0bbd7ae97749d60811d98ca7
Hazman Hatadi <hatadihazman@gmail.com>
2022-03-21 21:47:59 +0700

    l

--- frontend/footer.php
+++ frontend/footer.php
@@ -27,7 +27,7 @@
 								<div class="footer-socials-links mt-20">
 									<ul class="foo-socials text-center clearfix">
 
-										<li><a href="#" class="ico-facebook"><i class="fab fa-facebook-f"></i></a></li>
+										<li><a href="https://www.facebook.com/meditrans.id" class="ico-facebook"><i class="fab fa-facebook-f"></i></a></li>
 										<li><a href="#" class="ico-twitter"><i class="fab fa-twitter"></i></a></li>	
 										<li><a href="#" class="ico-google-plus"><i class="fab fa-google-plus-g"></i></a></li>
 										<li><a href="#" class="ico-tumblr"><i class="fab fa-tumblr"></i></a></li>			
