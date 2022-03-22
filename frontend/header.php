dfd4b6b60fc8181f0e8e04720822b6fe1bb994b1
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-03-23 00:17:58 +0700

    p

--- frontend/header.php
+++ frontend/header.php
@@ -97,8 +97,7 @@
 			    		<!-- Address -->
 			     		<div class="headertopleft">			     			
 			        		
-						<div class="address clearfix"><span>🕗 SCHEDULE MONDAY - FRIDAY 08.00 - 17.00 WIB                                    </span> 
-						<a  href="" style="color: black">✉ TEzzzzzzST</a>
+						<div class="address clearfix"><span>🕗 SCHEDULE MONDAY - FRIDAY 08.00 - 17.00 WIB</span> 
 			        </div>
 			     		</div>
 
@@ -108,7 +107,7 @@
 					    	<a class="googleicon" title="Instagram" href="https://www.instagram.com/meditransglobal.id/"><i class="fab fa-instagram"></i> <span class="mobiletext02">Instagram</span></a>
 					        <a class="linkedinicon" title="Linkedin" href="https://www.linkedin.com/company/meditransglobal/"><i class="fab fa-linkedin-in"></i> <span class="mobiletext02">Linkedin</span></a>
 					        <a class="facebookicon" title="Facebook" href="https://www.facebook.com/meditrans.id/"><i class="fab fa-facebook-f"></i> <span class="mobiletext02">Facebook</span></a>
-							
+							<a class="email" href="" style="color: black">✉ TEzzzzzzST</a>
 							
 							
 				      	</div>
