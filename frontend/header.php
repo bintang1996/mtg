a4e99bea88c49e4bed95dccf6d4f0805b5199ea7
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-03-23 00:13:45 +0700

    p

--- frontend/header.php
+++ frontend/header.php
@@ -97,7 +97,7 @@
 			    		<!-- Address -->
 			     		<div class="headertopleft">			     			
 			        		
-						<div class="address clearfix"><span>🕗 SCHEDULE MONDAY - FRIDAY 08.00 - 17.00 WIB</span> 
+						<div class="address clearfix"><span>🕗 SCHEDULE MONDAY - FRIDAY 08.00 - 17.00 WIB                                    </span> 
 						<a  href="" style="color: black">✉ TEzzzzzzST</a>
 			        </div>
 			     		</div>
