d7de4ba0d255331ebdb2c91a577776f730d5e5cc
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-03-23 00:01:15 +0700

    sd

--- frontend/header.php
+++ frontend/header.php
@@ -108,7 +108,7 @@
 					        <a class="linkedinicon" title="Linkedin" href="https://www.linkedin.com/company/meditransglobal/"><i class="fab fa-linkedin-in"></i> <span class="mobiletext02">Linkedin</span></a>
 					        <a class="facebookicon" title="Facebook" href="https://www.facebook.com/meditrans.id/"><i class="fab fa-facebook-f"></i> <span class="mobiletext02">Facebook</span></a>
 							<a class="email" href="" style="color: black">TEST</a>
-							<a href="" style="color: black;"><i class="fa fa-envelope"></i></a>
+							<a href="" style="color: black;"><i class="fa fa-envelope"></i>TESTt</a>
 							
 				      	</div>
 
