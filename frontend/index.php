078a87f313a8d9856f36c4a4a96ba259e463b22d
bintang1996 <fzlr.bintang@gmail.com>
2022-03-26 20:16:01 +0700

    pt

--- frontend/index.php
+++ frontend/index.php
@@ -120,7 +120,7 @@
 
 			<!-- ABOUT-5
 			============================================= -->
-			<section id="about-5" class="pt-100 about-section division" style="background-color:#281E78;">
+			<section id="about-5" class="pt-100 about-section division" style="background-color:#281E78; padding-top:50px;">
 				<div class="container" style="background-color:#281E78;">
 
 				
