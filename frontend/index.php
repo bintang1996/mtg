75ff0165a8de60d1aca78c6091125f58ce0cd3d2
bintang1996 <fzlr.bintang@gmail.com>
2022-03-26 18:36:10 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -243,7 +243,7 @@
 
 			</div>
 
-									</div>	
+		
 
 			<div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
 				<div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
