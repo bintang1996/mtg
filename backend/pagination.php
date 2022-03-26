8503515fd2aae2c0ce846ff8cbb764d70edd035a
bintang1996 <fzlr.bintang@gmail.com>
2022-03-26 21:08:38 +0700

    z

--- backend/pagination.php
+++ backend/pagination.php
@@ -6,38 +6,38 @@
 
 
 
-                            <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=1"><<</a></li>
+                            <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=1"><<</a></li>
                             <?php $ggg1=$page-3; if($ggg1>0){?>
 
-                            <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page-3; ?>"><?php echo $page-3; ?></a></li> <?php } ?>
+                            <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page-3; ?>"><?php echo $page-3; ?></a></li> <?php } ?>
                             <?php $ggg2=$page-2; if($ggg2>0){?>
-                            <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page-2; ?>"><?php echo $page-2; ?></a></li><?php } ?>
+                            <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page-2; ?>"><?php echo $page-2; ?></a></li><?php } ?>
                             <?php $ggg3=$page-1; if($ggg3>0){?>
                             
-                            <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page-1; ?>"><?php echo $page-1; ?></a></li>
+                            <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page-1; ?>"><?php echo $page-1; ?></a></li>
                             <?php } ?>
 
                             
-                            <li class="page-item active"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page; ?>"><?php echo $page; ?></a></li>
+                            <li class="page-item active"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page; ?>"><?php echo $page; ?></a></li>
                          
                             <?php $ggg3=$page+1; if($ggg3<=$pages){?>
-                            <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+1; ?>"><?php echo $page+1; ?></a></li><?php } ?>
+                            <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+1; ?>"><?php echo $page+1; ?></a></li><?php } ?>
                             
                             <?php $ggg3=$page+2; if($ggg3<=$pages){?>
-                                <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+2; ?>"><?php echo $page+2; ?></a></li><?php } ?>
+                                <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+2; ?>"><?php echo $page+2; ?></a></li><?php } ?>
                                 <?php $ggg3=$page+3; if($ggg3<=$pages){?>
-                                <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+3; ?>"><?php echo $page+3; ?></a></li><?php } ?>
+                                <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+3; ?>"><?php echo $page+3; ?></a></li><?php } ?>
                                 <?php $ggg3=$page+4; if($ggg3<=$pages){?>
-                                <li class="page-item"><a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+4; ?>"><?php echo $page+4; ?></a></li><?php } ?>
+                                <li class="page-item"><a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+4; ?>"><?php echo $page+4; ?></a></li><?php } ?>
                        
                             
                             
                             <li class="page-item">
                                 
-                            <a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+1; ?>">></i></a>
+                            <a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $page+1; ?>">></i></a>
                                         </li>
                             <li class="page-item">
-                            <a class="page-link" href="product-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $pages; ?>">>></i></a>
+                            <a class="page-link" href="navbar-page.php?cari=<?php echo $cari;?>&halaman=<?php echo $pages; ?>">>></i></a>
                            
                         </li>
          
