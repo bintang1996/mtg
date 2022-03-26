8f82b9f3897aa98fafbd02ee554c6809f6378efa
bintang1996 <fzlr.bintang@gmail.com>
2022-03-26 18:42:39 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -219,14 +219,14 @@
 
 			<div class="mySlides">
 
-<table class="table" style="border:none;"><tr><td>
+			<table class="table" style="border:none;">
+			<tr><td>
 			<img class="img-fluid" src="images/pediatrics_700x700.jpg" alt="tab-image">
-									</td><td>
-		
+			</td>
+			<td>	
 			<p>MEncoba bertahan tapi tak gege</p>
-									</td>
-
-</table>			
+			</td>
+			</table>			
 			</div>
 			
 
@@ -236,9 +236,9 @@
 			<div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
 				<div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
 				<div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
-				<span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
-				<span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
-				<span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+				<span class="w3-badge demo w3-border w3-transparent w3-hover-red" onclick="currentDiv(1)"></span>
+				<span class="w3-badge demo w3-border w3-transparent w3-hover-red" onclick="currentDiv(2)"></span>
+				<span class="w3-badge demo w3-border w3-transparent w3-hover-red" onclick="currentDiv(3)"></span>
 			</div>
 			</div>
 
