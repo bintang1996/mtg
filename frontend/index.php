8417858361971b5e7d8449ff71bf52178d95013c
bintang1996 <fzlr.bintang@gmail.com>
2022-03-26 14:10:42 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -53,8 +53,8 @@
 
 
 						<!-- ABOUT BOX #2 -->
-						<div id="abox-2" class="col-md-6 col-lg-4" style="margin-right:20px; border-radius:10%; ">
-							<div class="abox-1 white-color">
+						<div id="abox-2" class="col-md-12 col-lg-4" style="margin-right:20px; border-radius:10%; ">
+							<div class="abox-1 white-color" style="padding-right:20px;" >
 
 								<h5 class="h5-md">Solusi Kerjasama Operasional (KSO)</h5>
 
@@ -70,7 +70,7 @@
 
 
 						<!-- ABOUT BOX #3 -->
-						<div id="abox-3" class="col-md-6 col-lg-4" style="margin-right:20px; border-radius:10%;">
+						<div id="abox-3" class="col-md-12 col-lg-4" style="margin-right:20px; border-radius:10%;">
 							<div class="abox-1 white-color">
 
 								<!-- Title -->
@@ -89,7 +89,7 @@
 
 
 						<!-- ABOUT BOX #4 -->
-						<div id="abox-4" class="col-md-6 col-lg-3" style="border-radius:10%;">
+						<div id="abox-4" class="col-md-12 col-lg-4" style="border-radius:10%;">
 							<div class="abox-1 white-color">
 
 								<!-- Title -->
