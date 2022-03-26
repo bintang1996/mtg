c031e8d72fc93419cfa52d34b0f9c5c7a414eb19
bintang1996 <fzlr.bintang@gmail.com>
2022-03-26 23:09:17 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -277,9 +277,9 @@
 			<button class="w3-button w3-display-right w3-red" onclick="plusDivs(1)">&#10095;</button>
 			<div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
 			
-				<span class="w3-badge demo w3-border w3-red w3-hover-red" onclick="currentDiv(1)"></span>
-				<span class="w3-badge demo w3-border w3-red w3-hover-red" onclick="currentDiv(2)"></span>
-				<span class="w3-badge demo w3-border w3-red w3-hover-red" onclick="currentDiv(3)"></span>
+				<span class="w3-badge demo w3-border w3-white w3-hover-red" onclick="currentDiv(1)"></span>
+				<span class="w3-badge demo w3-border w3-white w3-hover-red" onclick="currentDiv(2)"></span>
+				<span class="w3-badge demo w3-border w3-white w3-hover-red" onclick="currentDiv(3)"></span>
 			</div>
 			</div>
 
@@ -305,10 +305,10 @@
 					x[i].style.display = "none";  
 				}
 				for (i = 0; i < dots.length; i++) {
-					dots[i].className = dots[i].className.replace(" w3-white", "");
+					dots[i].className = dots[i].className.replace(" w3-red", "");
 				}
 				x[slideIndex-1].style.display = "block";  
-				dots[slideIndex-1].className += " w3-white";
+				dots[slideIndex-1].className += " w3-red";
 				}
 				</script>
 
