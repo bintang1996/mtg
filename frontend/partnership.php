044aabfb89a021bf74f5115f70b249950e951b6e
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-03-28 16:31:53 +0700

    partnership breadcrumb

--- frontend/partnership.php
+++ frontend/partnership.php
@@ -8,16 +8,8 @@
 						<div class="col">
 							<div class=" breadcrumb-holder">
 
-								<!-- Breadcrumb Nav -->
-								<nav aria-label="breadcrumb">
-								  	<ol class="breadcrumb">
-								    	<li class="breadcrumb-item"><a href="index.html">Home</a></li>
-								    	<li class="breadcrumb-item active" aria-current="page">Who We Are</li>
-								  	</ol>
-								</nav>
-
 								<!-- Title -->
-								<h4 class="h4-sm steelblue-color">Who We Are</h4>
+								<h4 class="h4-sm steelblue-color">Partnership</h4>
 
 							</div>
 						</div>
