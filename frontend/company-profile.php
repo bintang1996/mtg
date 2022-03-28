bb6b427e8679e9fbf46b79ef1f032c72832a6857
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-03-28 21:38:38 +0700

    edit visi misi

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -359,13 +359,7 @@
 
 							</div>	
 						</div>	
-					</div>	  <!-- End row -->
-				</div>	   <!-- END TEXT BLOCK -->	
 
-
-				<!-- TEXT BLOCK -->		
-				<div class="container">
-					<div class="row d-flex align-items-center">	
 						<div class="col-md-6">
 							<div class="info-9-table white-color wow fadeInUp" data-wow-delay="0.4s">
 
@@ -407,7 +401,7 @@
 								</p>
 
 							</div>	
-						</div>	
+						</div>
 					</div>	  <!-- End row -->
 				</div>	   <!-- END TEXT BLOCK -->	
 
