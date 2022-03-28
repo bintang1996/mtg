00c8bec539dedad441b7651219e864d4f6781267
bintang1996 <fzlr.bintang@gmail.com>
2022-03-28 11:53:07 +0700

    z

--- frontend/assets/pages/jquery.jvectormap.init.js
+++ frontend/assets/pages/jquery.jvectormap.init.js
@@ -11,7 +11,8 @@
 	};
 
 
-
+	var zoomSettings = {scale: 3, lat: 0.789275, lng: 113.921326, animate: true};
+	mapObj.setFocus(zoomSettings);
 
 	
 	VectorMap.prototype.init = function() {
@@ -52,51 +53,12 @@
 				latLng : [0.33, 6.73],
 				name : 'São Tomé and Príncipe'
 			}],
-			onRegionClick: function(e,  code,  isSelected,  selectedRegions){
-				$('#map').vectorMap('get','mapObject').setFocus({region: code});
-			  }
-		});
-
-
-
-		$('#india').vectorMap({
-			map : 'id_ID',
-			backgroundColor : '#87CEFA',
-			normalizeFunction : 'polynomial',
-			hoverOpacity : 0.7,
-			regionStyle : {
-				initial : {
-					fill : '#8aa678'
-				}
-			},
-			markerStyle: {
-                initial: {
-                    r: 9,
-                    'fill': '#7382b3',
-                    'fill-opacity': 0.9,
-                    'stroke': '#fff',
-                    'stroke-width' : 7,
-                    'stroke-opacity': 0.4
-                },
-
-                hover: {
-                    'stroke': '#fff',
-                    'fill-opacity': 1,
-                    'stroke-width': 1.5
-                }
-            },
 			
-			markers : 
-			[{
-				latLng : [-6.20, 106.81],
-				name : 'Jakarta'
-			}, 
-			{
-				latLng : [-7.25, 112.75],
-				name : 'Surabaya'
-			}]
 		});
 
+
+
+		
 		
 
 	},
