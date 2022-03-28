ef7f535b4da87ac9ee07df8f566f921a0db718eb
Hazman Hatadi <hatadihazman@gmail.com>
2022-03-28 22:22:01 +0700

    l

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -35,7 +35,7 @@
 						<!-- IMAGE BLOCK -->
 						<div class="col-lg-6">
 							<div class="info-1-img text-center mb-40 wow fadeInUp" data-wow-delay="0.6s">
-								<img class="img-fluid" src="images/meditrans-office.png" alt="info-image">
+								<img class="img-fluid" src="images/image-11.png" alt="info-image">
 							</div>
 						</div>
 
