3fb57ef672a8edb70d6eb13f0a51860198ee4e00
bintang1996 <fzlr.bintang@gmail.com>
2022-03-28 15:42:37 +0700

    a

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -1,5 +1,29 @@
 <?php include "header.php";?>
 
+		<!-- BREADCRUMB
+			============================================= -->
+			<div id="breadcrumb" class="division">
+				<div class="container">
+					<div class="row">						
+						<div class="col">
+							<div class=" breadcrumb-holder">
+
+								<!-- Breadcrumb Nav -->
+								<nav aria-label="breadcrumb">
+								  	<ol class="breadcrumb">
+								    	<li class="breadcrumb-item"><a href="index.html">Home</a></li>
+								    	<li class="breadcrumb-item active" aria-current="page">Who We Are</li>
+								  	</ol>
+								</nav>
+
+								<!-- Title -->
+								<h4 class="h4-sm steelblue-color">Who We Are</h4>
+
+							</div>
+						</div>
+					</div>  <!-- End row -->	
+				</div>	<!-- End container -->		
+			</div>	<!-- END BREADCRUMB -->	
 
 
 
