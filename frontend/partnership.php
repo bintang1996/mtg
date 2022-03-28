8de72c441c623994def0ecb409fff2183bdda9c2
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-03-28 16:44:42 +0700

    partnership breadcrumb

--- frontend/partnership.php
+++ frontend/partnership.php
@@ -8,6 +8,14 @@
 						<div class="col">
 							<div class=" breadcrumb-holder">
 
+								<!-- Breadcrumb Nav -->
+								<nav aria-label="breadcrumb">
+								  	<ol class="breadcrumb">
+								    	<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+								    	<li class="breadcrumb-item active" aria-current="page">Partnership</li>
+								  	</ol>
+								</nav>
+
 								<!-- Title -->
 								<h4 class="h4-sm steelblue-color">Partnership</h4>
 
@@ -16,5 +24,94 @@
 					</div>  <!-- End row -->	
 				</div>	<!-- End container -->		
 			</div>	<!-- END BREADCRUMB -->	
+
+            	<!-- INFO-1
+			============================================= -->
+			<section id="info-1" class="wide-60 info-section division">
+				<div class="container">
+					<div class="row d-flex align-items-center">
+
+						<!-- IMAGE BLOCK -->
+						<div class="col-lg-6">
+							<div class="info-1-img text-center mb-40 wow fadeInUp" data-wow-delay="0.6s">
+								<img class="img-fluid" src="images/image-11.png" alt="info-image">
+							</div>
+						</div>
+
+
+						<!-- TEXT BLOCK -->	
+						<div class="col-lg-6">
+							<div class="txt-block pc-30 mb-40 wow fadeInUp" data-wow-delay="0.4s">
+
+								<!-- Section ID -->	
+					 			<span class="section-id blue-color">Welcome to MedService</span>
+
+								<!-- Title -->
+								<h3 class="h3-md steelblue-color">Clinic with Innovative Approach to Treatment</h3>
+
+								<!-- Text -->
+								<p class="mb-30">An enim nullam tempor sapien gravida donec enim ipsum blandit
+								   porta justo integer odio velna vitae auctor integer congue magna at pretium 
+								   purus pretium ligula rutrum itae laoreet augue posuere and curae integer 
+								   congue leo metus mollis primis and mauris
+								</p>
+
+								<!-- Options List -->
+								<div class="row">
+
+									<div class="col-xl-6">
+
+										<!-- Option #1 -->
+										<div class="box-list m-top-15">							
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Nemo ipsam egestas volute and turpis dolores quaerat</p>							
+										</div>
+
+										<!-- Option #2 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Magna luctus tempor</p>				
+										</div>
+
+										<!-- Option #3 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">An enim nullam tempor at pretium purus blandit</p>				
+										</div>
+
+									</div>
+
+									<div class="col-xl-6">
+
+										<!-- Option #4 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Magna luctus tempor blandit a vitae suscipit mollis</p>				
+										</div>
+
+										<!-- Option #5 -->
+										<div class="box-list m-top-15">							
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Nemo ipsam egestas volute turpis dolores quaerat</p>							
+										</div>
+
+										<!-- Option #6 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">An enim nullam tempor</p>				
+										</div>
+
+									</div>
+
+								</div>	<!-- End Options List -->
+
+							</div>
+						</div>	<!-- END TEXT BLOCK -->	
+
+					
+						
+					</div>    <!-- End row -->
+				</div>	   <!-- End container -->
+			</section>	<!-- END INFO-1 -->
 
 <?php include "footer.php";?>
