71a9b3ea68663af0e7f54df235059bcc2d282291
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-03-29 00:17:12 +0700

    asa

---
+++ frontend/lala.php
@@ -0,0 +1,6 @@
+<?php include "header.php";?>
+
+
+<canvas class="mapboxgl-canvas" tabindex="0" aria-label="Map" width="3324" height="900" style="width: 1662px; height: 450px;"></canvas>
+
+<?php include "footer.php";?>
