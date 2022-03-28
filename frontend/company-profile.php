8f0948309ebc7a09c579ff09a977431a72a0c1d6
Hazman Hatadi <hatadihazman@gmail.com>
2022-03-28 16:47:05 +0700

    l

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -26,5 +26,94 @@
 			</div>	<!-- END BREADCRUMB -->	
 
 
+<!-- ABOUT-US-MEDITRANS
+			============================================= -->
+			<section id="info-1" class="wide-60 info-section division">
+				<div class="container">
+					<div class="row d-flex align-items-center">
+
+						<!-- IMAGE BLOCK -->
+						<div class="col-lg-6">
+							<div class="info-1-img text-center mb-40 wow fadeInUp" data-wow-delay="0.6s">
+								<img class="img-fluid" src="images/image-11.png" alt="info-image">
+							</div>
+						</div>
+
+
+						<!-- TEXT BLOCK -->	
+						<div class="col-lg-6">
+							<div class="txt-block pc-30 mb-40 wow fadeInUp" data-wow-delay="0.4s">
+
+								<!-- Section ID -->	
+					 			<span class="section-id blue-color">About Us</span>
+
+								<!-- Title -->
+								<h3 class="h3-md steelblue-color">Meditrans Global</h3>
+
+								<!-- Text -->
+								<p class="mb-30">PT. Meditrans Global (PT. MTG) berdiri sejak tahun 2012 di Surabaya, berawal dari bisnis penyedia barang dan jasa dan seiring waktu kami terus mengembangkan perusahaan ini ke bidang bidang bisnis lainnya.
+
+								Berlatar belakang pendiri perusahaan yang memiliki pengalaman dibidang peralatan penunjang kesehatan, Saat ini kami memfokuskan pengembangan perusahaan dalam hal pelayanan Barang dan Jasa penunjang kesehatan. Kami memiliki team yang solid untuk bekerjasama dengan Rumah Sakit ataupun perusahaan penyedia peralatan kesehatan baik principal ataupun pihak KSO untuk memberikan konsultasi dan pelaksanaan pekerjaan Pra Instalasi peralatan kesehatan seperti instalasi X-Ray, CT-Scan, MRI dan Cathlab.
+
+								Selain itu kami juga berpengalaman dalam hal Konstruksi dan Renovasi untuk ruangan Operasi, ruang radiologi yang membutuhkan instalasi anti radiasi (Timbal Pb) maupun struktur penguat lantai dan rangka atas. Team kami sudah sangat berpengalaman dikarenakan kami sudah banyak bekerjasama dengan perusahaan besar seperti Aneka Gas, Siemens Healthcare, GE Healthcare dan banyak Rumah Sakit Pemerintah ataupun Swasta.
+								</p>
+
+								<!-- Options List -->
+								<div class="row">
+
+									<div class="col-xl-6">
+
+										<!-- Option #1 -->
+										<div class="box-list m-top-15">							
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Nemo ipsam egestas volute and turpis dolores quaerat</p>							
+										</div>
+
+										<!-- Option #2 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Magna luctus tempor</p>				
+										</div>
+
+										<!-- Option #3 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">An enim nullam tempor at pretium purus blandit</p>				
+										</div>
+
+									</div>
+
+									<div class="col-xl-6">
+
+										<!-- Option #4 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Magna luctus tempor blandit a vitae suscipit mollis</p>				
+										</div>
+
+										<!-- Option #5 -->
+										<div class="box-list m-top-15">							
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">Nemo ipsam egestas volute turpis dolores quaerat</p>							
+										</div>
+
+										<!-- Option #6 -->
+										<div class="box-list">	
+											<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
+											<p class="p-sm">An enim nullam tempor</p>				
+										</div>
+
+									</div>
+
+								</div>	<!-- End Options List -->
+
+							</div>
+						</div>	<!-- END TEXT BLOCK -->	
+
+					
+						
+					</div>    <!-- End row -->
+				</div>	   <!-- End container -->
+			</section>	<!-- END ABOUT-US-MEDITRANS -->
 
 <?php include "footer.php";?>
