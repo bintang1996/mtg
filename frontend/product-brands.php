9cde1d7f9e29c2b78d680a1134c4ff08b67673be
bintang1996 <fzlr.bintang@gmail.com>
2022-03-29 08:49:12 +0700

    a

--- frontend/product-brands.php
+++ frontend/product-brands.php
@@ -92,8 +92,7 @@
 									<h5 class="h5-sm steelblue-color"><?php echo $d1['p_name']; ?></h5>
 									<span class="blue-color"><?php echo $d['cat_name']; ?></span>
 
-									<p class="p-sm grey-color">Donec vel sapien augue integer turpis cursus porta, mauris sed
-									   augue luctus magna dolor luctus ipsum neque
+									<p class="p-sm grey-color"><?php $rt=$d1['p_desc']; echo substr("$rt", 0, 100); ?>
 									</p>
 
 								</div>	
