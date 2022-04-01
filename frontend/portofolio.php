4dc75b90bb84d24c1016d263471c828b1c807cab
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-04-01 15:52:43 +0700

    Portofolio

---
+++ frontend/portofolio.php
@@ -0,0 +1,26 @@
+<?php include "header.php";?>
+<!-- BREADCRUMB
+			============================================= -->
+			<div id="breadcrumb" class="division">
+				<div class="container">
+					<div class="row">						
+						<div class="col">
+							<div class=" breadcrumb-holder">
+
+								<!-- Breadcrumb Nav -->
+								<nav aria-label="breadcrumb">
+								  	<ol class="breadcrumb">
+								    	<li class="breadcrumb-item"><a href="index.php">Home</a></li>
+								    	<li class="breadcrumb-item active" aria-current="page">Portofolio</li>
+								  	</ol>
+								</nav>
+
+								<!-- Title -->
+								<h4 class="h4-sm steelblue-color">Portofolio</h4>
+
+							</div>
+						</div>
+					</div>  <!-- End row -->	
+				</div>	<!-- End container -->		
+			</div>	<!-- END BREADCRUMB -->	
+<?php include "footer.php";?>
