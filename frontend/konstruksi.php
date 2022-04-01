47e5c0e780f130c139c95d28a10a3f7d12561c2f
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-04-01 14:56:14 +0700

    konstruksi

--- frontend/konstruksi.php
+++ frontend/konstruksi.php
@@ -99,6 +99,39 @@
 						
 						</div>    <!-- End row -->
 					</div>	<!-- END TOP ROW -->
+						<!-- SECOND ROW -->
+						<div class="top-row mb-80">
+						<div class="row d-flex align-items-center">
+							<!-- INFO TEXT -->	
+						<div class="col-lg-6">
+							<div class="txt-block pc-30 wow fadeInUp" data-wow-delay="0.4s">
+
+															<!-- Title -->
+								<h3 class="h3-md steelblue-color">Latar Belakang KSO</h3>
+
+								<!-- Text -->
+								<p>An enim nullam tempor sapien gravida donec pretium ipsum  porta justo integer at  odio
+								   velna vitae auctor integer congue magna purus pretium ligula rutrum luctus ultrice aliquam
+								   a augue suscipit
+								</p>
+
+								<!-- Text -->
+								<p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat
+								   dolor lacinia cubilia curae integer congue leo metus, eu mollislorem primis in orci integer
+								   metus mollis faucibus. An enim nullam tempor sapien gravida donec pretium and ipsum porta
+								   justo integer at velna vitae auctor integer congue
+								</p>
+							</div>
+						</div>	<!-- END TEXT BLOCK -->	
+								<!-- INFO IMAGE -->
+						<div class="col-lg-6">
+							<div class="info-4-img text-center wow fadeInUp" data-wow-delay="0.6s">
+								<img class="img-fluid" src="images/chief_doctor_700x800.jpg" alt="info-image">
+							</div>
+						</div>
+					</div>    <!-- End row -->
+				</div>	<!-- END SECOND ROW -->
+
                 </div>	   <!-- End container -->
 			</section>	<!-- END INFO-4 -->
 			</div>	<!-- END SERVICE DETAILS -->
