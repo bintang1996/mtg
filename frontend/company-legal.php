29def82ede5dca18ec2e0839256db240928b5753
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-04-03 11:43:20 +0700

    k

--- frontend/company-legal.php
+++ frontend/company-legal.php
@@ -39,11 +39,21 @@
 
 
 						<!-- INFO IMAGE -->
-						<div class="col-lg-6">
-							<div class="info-4-img text-center wow fadeInUp" data-wow-delay="0.6s">
-								<img class="img-fluid" src="images/chief_doctor_700x800.jpg" alt="info-image">
-							</div>
-						</div>
+						<!-- IMAGE #2 -->
+                        <div class="gallery-item pediatric">
+									<div class="hover-overlay"> 
+
+										<!-- Gallery Image -->
+										<img class="img-fluid" src="images/gallery/image-10.jpg" alt="galley-image" />			
+										<div class="item-overlay"></div>				
+												
+										<!-- Image Zoom -->		
+										<div class="image-zoom">
+											<a class="image-link" href="images/gallery/image-10.jpg" title=""><i class="fas fa-search-plus"></i></a>
+										</div> 
+
+									</div>	
+								</div>	<!-- END IMAGE #2 -->	
 
 
 						<!-- INFO TEXT -->	
