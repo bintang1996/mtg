161cfe2761dc61972e435ab9d1968c534d295e93
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-04-04 11:51:26 +0700

    zxz

--- frontend/data-legal.php
+++ frontend/data-legal.php
@@ -31,44 +31,6 @@
 			<td width="5%"></td>
 			</table>			
 			</div>
-			
-			<div class="mySlides">
-
-			<table class="table" width="90%" style="border:none; margin-bottom:0; margin-right:5%;">
-			<tr><td width="50%"  class="text-center align-center">
-			<img class="img-fluid" width="70%" src="images/pediatrics_700x700.jpg" alt="tab-image">
-			</td>
-			<td><br><br>
-			<h3 class="h3-md" style="color: #281E78;">SELAMAT DATANG DI MTG</h3>	
-			<p style="text-align:justify;">MPT. Meditrans Global (PT. MTG) berdiri sejak tahun 2012 di Surabaya, berawal dari bisnis penyedia barang dan jasa dan seiring waktu kami terus mengembangkan perusahaan ini ke bidang bidang bisnis lainnya.</p>
-
-			<p style="text-align:justify;">Berlatar belakang pendiri perusahaan yang memiliki pengalaman dibidang peralatan penunjang kesehatan, Saat ini kami memfokuskan pengembangan perusahaan dalam hal pelayanan Barang dan Jasa penunjang kesehatan. 
-				Kami memiliki team yang solid untuk bekerjasama dengan Rumah Sakit ataupun perusahaan penyedia peralatan kesehatan baik principal ataupun pihak KSO untuk memberikan konsultasi dan pelaksanaan pekerjaan Pra Instalasi peralatan kesehatan seperti instalasi X-Ray, CT-Scan, MRI dan Cathlab.</p>
-				<br><a href="#" class="btn btn-md btn-blue blue-hover">Selengkapnya</a>
-			</td>
-			<td width="5%"></td>
-			</table>			
-			</div>
-
-			<div class="mySlides">
-
-			<table class="table" width="90%" style="border:none; margin-bottom:0; margin-right:5%;">
-			<tr><td width="50%"  class="text-center align-center">
-			<img class="img-fluid" width="70%" src="images/pediatrics_700x700.jpg" alt="tab-image">
-			</td>
-			<td><br><br>
-			<h3 class="h3-md" style="color: #281E78;">SELAMAT DATANG DI MTGLOBAL</h3>	
-			<p style="text-align:justify;">MPT. Meditrans Global (PT. MTG) berdiri sejak tahun 2012 di Surabaya, berawal dari bisnis penyedia barang dan jasa dan seiring waktu kami terus mengembangkan perusahaan ini ke bidang bidang bisnis lainnya.</p>
-
-			<p style="text-align:justify;">Berlatar belakang pendiri perusahaan yang memiliki pengalaman dibidang peralatan penunjang kesehatan, Saat ini kami memfokuskan pengembangan perusahaan dalam hal pelayanan Barang dan Jasa penunjang kesehatan. 
-				Kami memiliki team yang solid untuk bekerjasama dengan Rumah Sakit ataupun perusahaan penyedia peralatan kesehatan baik principal ataupun pihak KSO untuk memberikan konsultasi dan pelaksanaan pekerjaan Pra Instalasi peralatan kesehatan seperti instalasi X-Ray, CT-Scan, MRI dan Cathlab.</p>
-				<br><a href="#" class="btn btn-md btn-blue blue-hover">Selengkapnya</a>
-			</td>
-			<td width="5%"></td>
-
-			
-			</table>			
-			</div>
 		
 			<button class="w3-button w3-display-left w3-red" onclick="plusDivs(-1)">&#10094;</button>
 			<button class="w3-button w3-display-right w3-red" onclick="plusDivs(1)">&#10095;</button>
