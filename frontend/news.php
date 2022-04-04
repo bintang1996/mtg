3870be8ea8fbe5f6bb0444bbe7a0f708bb10252a
bintang1996 <fzlr.bintang@gmail.com>
2022-04-05 00:56:25 +0700

    s

--- frontend/news.php
+++ frontend/news.php
@@ -119,8 +119,8 @@
 							$sql2 = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM p_image where a_id='$x' limit 1") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
 							while($d2=mysqli_fetch_array($sql2)){
 							?> 
-									<img class="img-fluid" width="210" src="assets/imgs/shop/<?php echo $d2['img_name']; ?>" alt="doctor-foto">	
-							<?php } ?>
+										<div class="gambar" style="background-image: url('assets/imgs/shop/<?php echo $d2['img_name']; ?>');"></div>
+		<?php } ?>
 
 										</div>
 										
