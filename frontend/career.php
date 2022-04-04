02af7bd5e62b9ca65776afc94d89301f61d80cad
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-04-04 22:38:06 +0700

    career

--- frontend/career.php
+++ frontend/career.php
@@ -53,7 +53,7 @@
                 <div class="options-list">
 										<div class="row">
 
-											<div class="col-xl-3">
+											<div class="col-xl-4">
 
 												<!-- Option #1 -->
 												<div class="box-list m-top-15">							
@@ -62,7 +62,7 @@
 												</div>
 
 												<!-- Option #2 -->
-												<div class="box-list">	
+												<div class="box-list m-top-5">	
 													<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
 													<p class="p-sm">Lowongan 2</p>				
 												</div>
@@ -75,7 +75,7 @@
 
 											</div>
 
-											<div class="col-xl-3">
+											<div class="col-xl-4">
 
 												<!-- Option #4 -->
 												<div class="box-list">	
@@ -96,7 +96,7 @@
 												</div>
 
 											</div>
-                                            <div class="col-xl-3">
+                                            <div class="col-xl-4">
 
 												<!-- Option #7 -->
 												<div class="box-list">	
