6fdc90b19970a865e71d3d425e79488d1468a5f5
bintang1996 <fzlr.bintang@gmail.com>
2022-04-05 02:22:54 +0700

    s

--- frontend/index.php
+++ frontend/index.php
@@ -335,7 +335,7 @@
 					<div class="row">	
 						<div class="col-lg-10 offset-lg-1 section-title">		
 							<!-- Title 	-->	
-							<h3 class="h3-md steelblue-color" style="color: #281E78;"><b>PRODUCT BRAND</b></h3>								
+							<h3 class="h3-md steelblue-color" style="color: #281E78;"><b>Product Brand</b></h3>								
 						</div> 
 					</div>
 					<div class="row d-flex align-items-center text-center">
@@ -427,7 +427,7 @@
 					<div class="row">	
 						<div class="col-lg-10 offset-lg-1 section-title" style="margin-bottom:30px;">		
 							<!-- Title 	-->	
-							<h3 class="h3-md steelblue-color" style="color: #281E78;"><b>NEWS, EDUCATION & OUR STORIES</b></h3>								
+							<h3 class="h3-md steelblue-color" style="color: #281E78;"><b>News, Education & Our Stories</b></h3>								
 						</div> 
 					</div>
 				
@@ -505,7 +505,7 @@
 				<div class="row">	
 						<div class="col-lg-10 offset-lg-1 section-title" style="margin-bottom:30px;">		
 							<!-- Title 	-->	
-							<h3 class="h3-md steelblue-color" style="color: #281E78;"><b>OFFICE & SITE LOCATION</b></h3>								
+							<h3 class="h3-md steelblue-color" style="color: #281E78;"><b>Office & Site Location</b></h3>								
 						</div> 
 					</div>
 					<div class="row">
