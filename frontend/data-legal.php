37e55f4afbb2b0e610b2d9376458254500957506
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-04-04 13:22:00 +0700

    xss

--- frontend/data-legal.php
+++ frontend/data-legal.php
@@ -23,7 +23,12 @@
 
 
 			<table class="table" width="90%" style="border:none; margin-bottom:0; margin-right:5%;">
-			<tr><td width="50%"  class="text-center align-center">
+
+			<tr>
+            <td><br><br>
+			<h3 class="h3-md" style="color: #281E78;"><?php echo $d['link_slider']; ?></h3>
+			</td>
+            <td width="50%"  class="text-center align-center">
 			<img style="width:250px;  max-width:250px; max-height: 300px; display: inline-block;"class="img-fluid"  src="assets/imgs/banner/<?php echo $d['slider_img']; ?>" alt="tab-image">
             
 			</td>
