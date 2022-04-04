0703ee12bd4ea0186bb97f2997e6e63a20dbfa1d
bintang1996 <fzlr.bintang@gmail.com>
2022-04-05 03:21:03 +0700

    l

--- frontend/article-details.php
+++ frontend/article-details.php
@@ -171,15 +171,6 @@
 						<aside id="sidebar" class="col-lg-4">
 
 
-							<!-- SEARCH FIELD --> 
-							<div id="search-field" class="sidebar-div mb-50">								
-								<div class="input-group mb-3">
-								  	<input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-field">
-								 	<div class="input-group-append">
-								    	<button class="btn" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
-								 	</div>
-								</div>
-							</div>
 							
 
 
