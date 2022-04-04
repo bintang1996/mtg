ae26f6667e9e518d9c48e56ad72eca336e6f0913
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-04-04 22:42:36 +0700

    career

--- frontend/career.php
+++ frontend/career.php
@@ -62,9 +62,9 @@
 												</div>
 
 												<!-- Option #2 -->
-												<div class="box-list m-top-15">	
+												<div class="box-list" style="margin-top: 20px;">	
 													<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
-													<p class="p-sm">Lowongan 2</p>				
+													<p>Lowongan 2</p>				
 												</div>
 
 												<!-- Option #3 -->
