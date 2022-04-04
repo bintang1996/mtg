7ee0048a6d7e1126258a7fa2dc9cd830fa5c61e8
bintang1996 <fzlr.bintang@gmail.com>
2022-04-04 16:12:55 +0700

    s

--- frontend/company-legal.php
+++ frontend/company-legal.php
@@ -29,7 +29,7 @@
             
 			<!-- INFO-4
 			============================================= -->
-			<section id="info-4" class="wide-100 info-section division" style="background-color: white;">
+			<section id="info-4" class="wide-100 info-section division" style="background-color: white; margin-bottom:40px;">
 				<div class="container">
 
 
