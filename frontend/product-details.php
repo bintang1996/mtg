70f0ad88146f709563660efce6e81a852a8f1fd9
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-04-04 20:59:39 +0700

    lkjhgf

--- frontend/product-details.php
+++ frontend/product-details.php
@@ -25,6 +25,69 @@
 			</div>	<!-- END BREADCRUMB -->	
 
 
+			<!-- INFO-4
+			============================================= -->
+			<section id="info-4" class="wide-100 info-section division">
+				<div class="container">
+
+
+					<!-- TOP ROW -->
+					<div class="top-row mb-80">
+						<div class="row d-flex align-items-center">
+
+
+						<!-- INFO IMAGE -->
+						<div class="col-lg-6">
+							<div class="info-4-img text-center wow fadeInUp" data-wow-delay="0.6s">
+								<img class="img-fluid" src="images/chief_doctor_700x800.jpg" alt="info-image">
+							</div>
+						</div>
+
+
+						<!-- INFO TEXT -->	
+						<div class="col-lg-6">
+							<div class="txt-block pc-30 wow fadeInUp" data-wow-delay="0.4s">
+
+								<!-- Section ID -->	
+					 			<span class="section-id blue-color">Welcome to MedService</span>
+
+								<!-- Title -->
+								<h3 class="h3-md steelblue-color">Clinic with Innovative Approach to Treatment</h3>
+
+								<!-- Text -->
+								<p>An enim nullam tempor sapien gravida donec pretium ipsum  porta justo integer at  odio
+								   velna vitae auctor integer congue magna purus pretium ligula rutrum luctus ultrice aliquam
+								   a augue suscipit
+								</p>
+
+								<!-- Text -->
+								<p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat
+								   dolor lacinia cubilia curae integer congue leo metus, eu mollislorem primis in orci integer
+								   metus mollis faucibus. An enim nullam tempor sapien gravida donec pretium and ipsum porta
+								   justo integer at velna vitae auctor integer congue
+								</p>
+
+								<!-- Singnature -->
+								<div class="singnature mt-35">
+
+									<!-- Text -->
+									<p class="p-small mb-15">Randon Pexon, Head of Clinic</p>
+
+									<!-- Singnature Image -->
+									<!-- Recommended sizes for Retina Ready displays is 400x68px; -->
+									<img class="img-fluid" src="images/signature.png" width="200" height="34" alt="signature-image" />	
+
+								</div>
+
+							</div>
+						</div>	<!-- END TEXT BLOCK -->	
+
+						
+						</div>    <!-- End row -->
+					</div>	<!-- END TOP ROW -->
+
+				</div>	   <!-- End container -->
+			</section>	<!-- END INFO-4 -->
 
 
 
