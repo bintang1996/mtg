99601dabc6b4a23116355263b59bd551e20eba90
Hazman Hatadi <hatadihazman@gmail.com>
2022-04-12 06:25:17 +0700

    l

--- frontend/konstruksi.php
+++ frontend/konstruksi.php
@@ -37,19 +37,15 @@
 				 			<div class="col-lg-12 text-left">
 
 				 				<!-- Title -->
-								<h3 class="h3-xl blue-color">Magnetic Resonance Imaging</h3>
+								<h3 class="h3-xl blue-color">Layanan Konstruksi</h3>
 								
 								<!-- Text -->
-								<p class="p-lg">Ligula risus auctor tempus dolor feugiat, felis lacinia risus interdum auctor
-								   id viverra dolor iaculis luctus bibendum luctus neque rhoncus ipsum tempor varius iaculis at
-								   luctus neque rhoncus ipsum tempor varius cubilia laoreet augue vitae laoreet augue undo cubilia 
-								   feugiat suscipit emper lacus
-								</p>
-				 				<!-- Text -->
-								<p class="p-lg">Ligula risus auctor tempus dolor feugiat, felis lacinia risus interdum auctor
-								   id viverra dolor iaculis luctus bibendum luctus neque rhoncus ipsum tempor varius iaculis at
-								   luctus neque rhoncus ipsum tempor varius cubilia laoreet augue vitae laoreet augue undo cubilia 
-								   feugiat suscipit emper lacus
+								<p class="p-lg">Layanan konstruksi dan renovasi Meditrans di handle oleh 
+									tim yang berpengalaman dan berkompeten di bidangnya. Antara lain penyediaan 
+									pintu anti radiasi, sandwich panel, pemasangan vinyl, MOT, Kaca Pb, timbalisasi, dan 
+									raised floor. Hingga saat ini, Meditrans melayani jasa konstruksi dan renovasi di 
+									puluhan Rumah Sakit baik RS Pemerintah maupun RS Swasta.
+
 								</p>
 
 				 			</div>
@@ -81,20 +77,21 @@
 							<div class="txt-block pc-30 wow fadeInUp" data-wow-delay="0.4s">
 
 															<!-- Title -->
-								<h3 class="h3-md steelblue-color">Latar Belakang KSO</h3>
+								<h3 class="h3-md steelblue-color">Konstruksi</h3>
 
 								<!-- Text -->
-								<p>An enim nullam tempor sapien gravida donec pretium ipsum  porta justo integer at  odio
-								   velna vitae auctor integer congue magna purus pretium ligula rutrum luctus ultrice aliquam
-								   a augue suscipit
+								<p>
+									- Timbalisasi ruangan Radiologi<br>
+									- Penyediaan Kaca Pb, Frame Kaca dan Pemasangannya
+									- Menyediakan Pintu Timbal atau Anti radiasi dengan berbagai macam tipe dan bahan (Finishing HPL, Stainles Steel baik manual maupun otomatis atau Pintu Harmetic)<br>
+									- Finishing Ruangan dengan berbagai macam bahan (Partisi Gypsum, Kalsiboard maupun Sandwich Panel)<br>
+									- Penyediaan dan pemasangan lantai vynil<br>
+									- Pengerjaan penguatan rangka atas untuk penggunaan alat dan penguatan fondasi alat<br>
+									- Electrical dan Grounding Installation<br>
+									- Penyediaan dan pemasangan raised floor
+
 								</p>
 
-								<!-- Text -->
-								<p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat
-								   dolor lacinia cubilia curae integer congue leo metus, eu mollislorem primis in orci integer
-								   metus mollis faucibus. An enim nullam tempor sapien gravida donec pretium and ipsum porta
-								   justo integer at velna vitae auctor integer congue
-								</p>
 							</div>
 						</div>	<!-- END TEXT BLOCK -->	
 
@@ -109,19 +106,14 @@
 							<div class="txt-block pc-30 wow fadeInUp" data-wow-delay="0.4s">
 
 															<!-- Title -->
-								<h3 class="h3-md steelblue-color">Latar Belakang KSO</h3>
+								<h3 class="h3-md steelblue-color">Renovasi Ruang Operasi (MOT)</h3>
 
 								<!-- Text -->
-								<p>An enim nullam tempor sapien gravida donec pretium ipsum  porta justo integer at  odio
-								   velna vitae auctor integer congue magna purus pretium ligula rutrum luctus ultrice aliquam
-								   a augue suscipit
-								</p>
-
-								<!-- Text -->
-								<p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat
-								   dolor lacinia cubilia curae integer congue leo metus, eu mollislorem primis in orci integer
-								   metus mollis faucibus. An enim nullam tempor sapien gravida donec pretium and ipsum porta
-								   justo integer at velna vitae auctor integer congue
+								<p>
+									- Hepa Filter<br>
+									- Instrument Ruang Operasi: Meja operasi, pass box, film viewer, display information, pendant, vynil lantai, dll<br>
+									- Finishing dinding ruang operasi dengan material lokal maupun import<br>
+									- pintu hermatic dnegan mesin China, Jepang, maupun Eropa
 								</p>
 							</div>
 						</div>	<!-- END TEXT BLOCK -->	
