b46db50744e880e29f0661907c377276ae402a04
bintang1996 <fzlr.bintang@gmail.com>
2022-04-18 20:01:18 +0700

    q

--- backend/career-list.php
+++ backend/career-list.php
@@ -113,7 +113,7 @@
            
            <td>
   <?php $sql3 = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM p_image where cv_id='$id'"); while($d3=mysqli_fetch_array($sql3)){?>
-   <a target="_blank" href="../frontend/assets/imgs/shop/<?php echo $d3['img_name']; ?>" title="Download Document"> <p><?php $r=0; $r++; echo "attacment"; echo $r; ?></p></a>
+   <a target="_blank" href="../frontend/assets/imgs/shop/<?php echo $d3['img_name']; ?>" title="Download Document"> <p><?php $r=0; $r++; echo "Attacment"; echo " "; echo $r++; ?></p></a>
      <?php } ?>
     </td>
                                         
