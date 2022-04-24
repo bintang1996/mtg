0e4a84f40ea3bd0c8361edb837641c53486ca843
bintang1996 <fzlr.bintang@gmail.com>
2022-04-24 19:53:10 +0700

    a

--- frontend/company-legal.php
+++ frontend/company-legal.php
@@ -103,10 +103,18 @@
 
                 </div>	   <!-- End container -->
 			</section>	<!-- END INFO-4 -->
-<!-- INFO-6
-			============================================= -->
-			
-			<!-- INFO-6
-			============================================= -->
 
+
+
+	<section id="info-4" class="wide-100 info-section division" style="background-color: #f8f9fb; padding-top: 40px;">
+				<div class="container">
+                
+                <h4 class="h4-md" style="color: #281E78;font-family:'Lato'; text-align : center; margin-top:0px; font-weight:bolder;">Company Legal History</h4>
+                <br>
+
+				<?php include "milestone.php";?>
+
+
+				</div>	   <!-- End container -->
+			</section>	<!-- END INFO-4 -->
 <?php include "footer.php";?>
