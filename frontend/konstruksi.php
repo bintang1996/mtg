321e862bdcb48fac2f17b528bc60c8e12a8d9518
Mufidatun Nisa Nur Lailiyah <35131176+mufiidatunnisaa@users.noreply.github.com>
2022-04-24 17:38:36 +0700

    sds

--- frontend/konstruksi.php
+++ frontend/konstruksi.php
@@ -81,7 +81,7 @@
 
 								<!-- Text -->
 								<p>
-								<i class="icon fa fa-circle">- Timbalisasi ruangan Radiologi</i><br>
+								<i class="fa fa-circle">- Timbalisasi ruangan Radiologi</i><br>
 									- Penyediaan Kaca Pb, Frame Kaca dan Pemasangannya
 									- Menyediakan Pintu Timbal atau Anti radiasi dengan berbagai macam tipe dan bahan (Finishing HPL, Stainles Steel baik manual maupun otomatis atau Pintu Harmetic)<br>
 									- Finishing Ruangan dengan berbagai macam bahan (Partisi Gypsum, Kalsiboard maupun Sandwich Panel)<br>
