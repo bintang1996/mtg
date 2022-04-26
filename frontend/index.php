f987f138c8ccab090bc2cb40dadd9dafe31e16a6
bintang1996 <fzlr.bintang@gmail.com>
2022-04-26 08:53:42 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -424,7 +424,7 @@
 
 		
 			
-<div class="w3-content w3-display-container" style="max-width:100%; background-color:#fff;">
+<div class="w3-content w3-display-container" style="max-width:100%; background-color:#fff; padding-bottom: 50px;">
 
 <div class="row">	
 						<div class="col-lg-10 offset-lg-1 section-title">		
