741faf6de74d1edf7ab192f2a31506f7e2a9968a
bintang1996 <fzlr.bintang@gmail.com>
2022-04-26 08:45:58 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -474,31 +474,31 @@
 </div>
 <?php }} ?>
 	<script>
-	var slideIndex = 1;
-	showDivs(slideIndex);
+	var slideIndex1 = 1;
+	showDivs(slideIndex1);
 
 	function plusDivs(n) {
-	showDivs(slideIndex += n);
+	showDivs(slideIndex1 += n);
 	}
 
 	function currentDiv(n) {
-	showDivs(slideIndex = n);
+	showDivs(slideIndex1 = n);
 	}
 
 	function showDivs(n) {
 	var i;
 	var x = document.getElementsByClassName("mySlides1");
 	var dots = document.getElementsByClassName("demo1");
-	if (n > x.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = x.length}
+	if (n > x.length) {slideIndex1 = 1}
+	if (n < 1) {slideIndex1 = x.length}
 	for (i = 0; i < x.length; i++) {
 		x[i].style.display = "none";  
 	}
 	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" w3-white", "");
 	}
-	x[slideIndex-1].style.display = "block";  
-	dots[slideIndex-1].className += " w3-white";
+	x[slideIndex1-1].style.display = "block";  
+	dots[slideIndex1-1].className += " w3-white";
 	}
 	</script>
 
