7373b431f18dae5347f97d7bae18e54aefdd78cf
bintang1996 <fzlr.bintang@gmail.com>
2022-04-26 10:52:12 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -380,7 +380,7 @@
 
 					<!-- SECTION TITLE -->	
 					<div class="row">	
-						<div class="col-lg-10 offset-lg-1 section-title">		
+						<div class="col-lg-10 offset-lg-1 section-title" style="margin-bottom:30px;">		
 							<!-- Title 	-->	
 							<h3 class="h4-md steelblue-color" style="color: #281E78;"><b><b>PRODUCT BRAND</b></b></h3>								
 						</div> 
