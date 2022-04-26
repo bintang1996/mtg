62b80dc1c8088942205f42a5d04fc53148bd3480
bintang1996 <fzlr.bintang@gmail.com>
2022-04-26 09:19:18 +0700

    a

--- frontend/index.php
+++ frontend/index.php
@@ -466,9 +466,9 @@
 <button class="w3-button w3-display-right w3-red" onclick="plusDivs1(1)">&#10095;</button>
 <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
 
-	<span class="w3-badge demo1 w3-border w3-red w3-hover-red" onclick="currentDiv(1)"></span>
-	<span class="w3-badge demo1 w3-border w3-red w3-hover-red" onclick="currentDiv(2)"></span>
-	<span class="w3-badge demo1 w3-border w3-red w3-hover-red" onclick="currentDiv(3)"></span>
+	<span class="w3-badge demo1 w3-border w3-red w3-hover-red" onclick="currentDiv(4)"></span>
+	<span class="w3-badge demo1 w3-border w3-red w3-hover-red" onclick="currentDiv(5)"></span>
+	<span class="w3-badge demo1 w3-border w3-red w3-hover-red" onclick="currentDiv(6)"></span>
 
 
 
