5aa386252df26cb77e9ef26a25cfe9e2a7e07dca
Moh Zainur Rodhi <zainurrodhi@gmail.com>
2022-05-09 22:10:06 +0700

    coba

--- frontend/company-legal.php
+++ frontend/company-legal.php
@@ -98,8 +98,28 @@
                 
                 <h4 class="h4-md" style="color: #281E78;font-family:'Lato'; text-align : center; margin-top:0px; font-weight:bolder;">COMPANY LEGAL HISTORY</h4>
                 <br>
-                <?php include "history-company.php";?>
+					<div class="col-lg-6">
+                		<?php include "history-company.php";?>
+					</div>
+					<div class="col-lg-6">
+						<div class="txt-block pc-30 wow fadeInUp" data-wow-delay="0.4s">
+							<!-- Title -->
+							<h3 class="h3-md steelblue-color">Lorem ipsum dolor sit amet</h3>
 
+							<!-- Text -->
+							<p>An enim nullam tempor sapien gravida donec pretium ipsum  porta justo integer at  odio
+							velna vitae auctor integer congue magna purus pretium ligula rutrum luctus ultrice aliquam
+							a augue suscipit
+							</p>
+
+							<!-- Text -->
+							<p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat
+							dolor lacinia cubilia curae integer congue leo metus, eu mollislorem primis in orci integer
+							metus mollis faucibus. An enim nullam tempor sapien gravida donec pretium and ipsum porta
+							justo integer at velna vitae auctor integer congue
+							</p>
+						</div>
+					</div>
 
                 </div>	   <!-- End container -->
 			</section>	<!-- END INFO-4 -->
