f485c315e5b17cb011a1d2617e910e6be7043f74
bintang1996 <fzlr.bintang@gmail.com>
2022-05-12 22:57:11 +0700

    s

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -446,34 +446,34 @@
 
 					<div class="row col-md-12">
 
-					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
-    				<img width="150px" style="max-height: 50px;" src="images/networks/Picture1.png">
+					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 50;">
+    				<img style="max-height: 50px;" src="images/networks/Picture1.png">
   					</div>
 
-					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
-    				<img width="150px" style="max-height: 50px;" src="images/networks/Picture2.png">
-  					</div>
-
-
-					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
-    				<img width="150px" style="max-height: 50px;" src="images/networks/Picture3.png">
-  					</div>
-
-					  <div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
-    				<img width="150px" style="max-height: 50px;" src="images/networks/Picture4.png">
-  					</div>
-
-					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
-    				<img width="150px" style="max-height: 50px;" src="images/networks/Picture5.png">
+					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 50;">
+    				<img  style="max-height: 50px;" src="images/networks/Picture2.png">
   					</div>
 
 
-					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
-    				<img width="150px" style="max-height: 50px;" src="images/networks/Picture6.png">
+					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 50;">
+    				<img  style="max-height: 50px;" src="images/networks/Picture3.png">
   					</div>
 
 					  <div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
-    				<img width="150px" style="max-height: 50px;" src="images/networks/Picture7.png">
+    				<img  style="max-height: 50px;" src="images/networks/Picture4.png">
+  					</div>
+
+					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
+    				<img  style="max-height: 50px;" src="images/networks/Picture5.png">
+  					</div>
+
+
+					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
+    				<img style="max-height: 50px;" src="images/networks/Picture6.png">
+  					</div>
+
+					  <div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
+    				<img style="max-height: 50px;" src="images/networks/Picture7.png">
   					</div>
 
 					<div class="panel-body" style="text-align: center; overflow: hidden; padding: 15;">
