bf763d3bd0d22ff214070ad280c8c219fce8291f
bintang1996 <fzlr.bintang@gmail.com>
2022-05-16 23:12:15 +0700

    s

--- frontend/product-details.php
+++ frontend/product-details.php
@@ -93,15 +93,59 @@
 
 					<nav>
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Deskripsi Produk</a>
+    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Kontak</a>
+    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Brosur</a>
   </div>
 </nav>
 <div class="tab-content" id="nav-tabContent">
-  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
-  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+
+  <?php 
+                  $id = $_GET['id'];      
+                  $sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM product where id='$id'") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
+                  while($d=mysqli_fetch_array($sql)){
+                            
+                ?> 
+					 			<span class="section-id blue-color">Product</span>
+                
+								<!-- Title -->
+								<h3 class="h3-md steelblue-color"><?php echo $d['p_name']; ?></h3>
+
+								<!-- Text -->
+								<p><?php echo $d['p_desc']; ?>
+								</p>
+
+                <h5 class=" steelblue-color">Spesifikasi</h5>
+								<!-- Text -->
+								<p><?php echo $d['spec']; ?>
+								</p>
+              
+                <?php } ?>
+
+
+  </div>
+  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+
+  <br><p>Informasi Produk Lebih Lanjut</p>
+                <a href="contact-us.php" class="btn btn-md btn-blue blue-hover">Hubungi Kami</a>
+
+  </div>
+  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+
+
+
+  <?php       
+           
+            $id=$_GET['id'];
+				$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM p_image where p_id='$id' and img_name LIKE '%.pdf%' ") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
+				while($d=mysqli_fetch_array($sql)){
+			?>
+
+<a href="assets/imgs/shop/<?php echo $d['img_name']; ?>">Download <?php echo $d['img_name']; ?></a> 
+
+<?php } ?>
+  </div>
 </div>
 
 
