8ef0308b4ba161f71537a0b2e0ee6f3e21d90938
bintang1996 <fzlr.bintang@gmail.com>
2022-05-16 23:05:31 +0700

    s

--- frontend/product-details.php
+++ frontend/product-details.php
@@ -78,7 +78,20 @@
 
 				
 						
-						<nav>
+
+
+
+
+
+
+
+						</div>    <!-- End row -->
+					</div>	<!-- END TOP ROW -->
+
+
+
+
+					<nav>
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
     <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
     <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
@@ -92,12 +105,6 @@
 </div>
 
 
-
-
-
-
-						</div>    <!-- End row -->
-					</div>	<!-- END TOP ROW -->
 
 				</div>	   <!-- End container -->
 			</section>	<!-- END INFO-4 -->
