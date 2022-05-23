daead2b5ddc4ae77a24dd2a0482023ac1120f796
bintang1996 <fzlr.bintang@gmail.com>
2022-05-23 20:34:28 +0700

    s

--- frontend/index.php
+++ frontend/index.php
@@ -277,7 +277,7 @@
 						<!-- INFO IMAGE -->
 						<div class="col-lg-6">
 				
-			<img class="img-fluid" width="70%" src="images/meditrans-office-ps3.png" alt="tab-image">
+			<img class="img-fluid" width="100%" src="images/meditrans-office-ps3.png" alt="tab-image">
 			
 						</div>
 
