fce07a8c6f7b0081af1bee27602e3a613ba75aac
bintang1996 <fzlr.bintang@gmail.com>
2022-05-23 20:46:59 +0700

    s

--- frontend/index.php
+++ frontend/index.php
@@ -270,7 +270,7 @@
 
 
 					<!-- TOP ROW -->
-					<div class="top-row mb-80">
+					<div class="top-row mb-80" style="background-color:#f8f9fa;">
 						<div class="row d-flex align-items-center">
 
 
