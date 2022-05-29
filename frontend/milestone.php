b22380b23f9d4f615184c69b6a3f2129890ea2de
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 04:59:32 +0700

    s

--- frontend/milestone.php
+++ frontend/milestone.php
@@ -188,7 +188,7 @@
 						<li class="timeline-item">
 							<div class="timeline-badge primary"><i class="glyphicon glyphicon-check"></i></div>
 							<div class="timeline-panel">
-              <img style="max-height: 150px;" src="images/head/legal.jpg" alt="doctor-foto">	
+              <img style="max-height: 100px;" src="images/head/legal.jpg" alt="doctor-foto">	
               	<!-- Icon -->
 								
 											
