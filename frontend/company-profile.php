745c90f255be818927f416a0c48f635e6b2057c0
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 01:36:27 +0700

    d

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -108,7 +108,7 @@
 										<img class="img-fluid" src="images/Ibnu_Affan-photo.png" alt="doctor-foto">	
 											
 										<!-- Title -->
-										<div style="margin-right: 20; margin-left: 20;">
+										<div style="margin-right: 20px; margin-left: 20px;">
 										<h5 class="h5-xs steelblue-color">Pediatrics</h5>
 
 										<!-- Text -->
