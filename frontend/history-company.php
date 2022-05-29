936d83aea025f9225eea101c8931aca9caf0755d
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 03:23:30 +0700

    s

--- frontend/history-company.php
+++ frontend/history-company.php
@@ -63,12 +63,12 @@
 
                       
                     <!-- Title -->
-                    <div class="text-left" style="margin-right: 20px; margin-left: 20px;">
+                    <div class="text-left orange-hover" style="margin-right: 20px; margin-left: 20px;">
                     <h5 class="h5-xs steelblue-color">Ibnu Affan</h5>
 
                     <!-- Text -->
-                    Chief Executive Officer
-                    
+                    <p>Chief Executive Officer
+                    </p>
                     </div>
                    </div>
 
