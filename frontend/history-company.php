e4a3b955f1bc56cea0c58ac873979af0cf10bcfb
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 03:15:23 +0700

    A

--- frontend/history-company.php
+++ frontend/history-company.php
@@ -22,19 +22,19 @@
 										<!-- SERVICE BOX #1 -->
 										<div class="sbox-3 icon-sm" style="padding-top: 0; padding-left: 0; padding-right: 0;">
 
-<!-- Icon -->
+                    <!-- Icon -->
 
-  
-<!-- Title -->
-<div class="text-left" style="margin-right: 20px; margin-left: 20px;">
-<h5 class="h5-xs steelblue-color">Ibnu Affan</h5>
+                      
+                    <!-- Title -->
+                    <div class="text-left" style="margin-right: 20px; margin-left: 20px;">
+                    <h5 class="h5-xs steelblue-color">Ibnu Affan</h5>
 
-<!-- Text -->
-<p>Chief Executive Officer
-</p>
-</div>
-<br><a href="https://mtg.sticare.co.id/frontend/company-profile.php" class="btn btn-sm btn-orange">READ MORE</a>																										
-</div>
+                    <!-- Text -->
+                    <p>Chief Executive Officer
+                    </p>
+                    </div>
+                    <br><a href="https://mtg.sticare.co.id/frontend/company-profile.php" class="btn btn-sm btn-orange">READ MORE</a>																										
+                    </div>
 
 
 
@@ -62,19 +62,19 @@
 										<!-- SERVICE BOX #1 -->
 										<div class="sbox-3 icon-sm" style="padding-top: 0; padding-left: 0; padding-right: 0;">
 
-<!-- Icon -->
+                    <!-- Icon -->
 
-  
-<!-- Title -->
-<div class="text-left" style="margin-right: 20px; margin-left: 20px;">
-<h5 class="h5-xs steelblue-color">Ibnu Affan</h5>
+                      
+                    <!-- Title -->
+                    <div class="text-left" style="margin-right: 20px; margin-left: 20px;">
+                    <h5 class="h5-xs steelblue-color">Ibnu Affan</h5>
 
-<!-- Text -->
-<p>Chief Executive Officer
-</p>
-</div>
-<br><a href="https://mtg.sticare.co.id/frontend/company-profile.php" class="btn btn-sm btn-orange">READ MORE</a>																										
-</div>
+                    <!-- Text -->
+                    <p>Chief Executive Officer
+                    </p>
+                    </div>
+                    <br><a href="https://mtg.sticare.co.id/frontend/company-profile.php" class="btn btn-sm btn-orange">READ MORE</a>																										
+                    </div>
 
 
 
