8284505a3146439057d7822cddee5dacde1aeb8f
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 03:30:55 +0700

    s

--- frontend/history-company.php
+++ frontend/history-company.php
@@ -76,13 +76,13 @@
 
 </td><td>
 										<!-- SERVICE BOX #1 -->
-										<div class="sbox-3 icon-sm orange-hover" style="padding-top: 0; padding-left: 0; padding-right: 0;">
+										<div class="sbox-3 icon-sm orange-hover">
 
 										<!-- Icon -->
 								
 											
 										<!-- Title -->
-										<div class="text-left" style="margin-right: 20px; margin-left: 20px;">
+										<div class="text-left">
 										<h5 class="h5-xs steelblue-color">Ibnu Affan</h5>
 
 										<!-- Text -->
