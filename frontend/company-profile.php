88b72771e4456146c3fae995256e637aaf421c8e
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 01:37:12 +0700

    a

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -125,12 +125,14 @@
 										<img class="img-fluid" src="images/Ibnu_Affan-photo.png" alt="doctor-foto">	
 											
 										<!-- Title -->
+										<div style="margin-right: 20px; margin-left: 20px;">
 										<h5 class="h5-xs steelblue-color">Pediatrics</h5>
 
 										<!-- Text -->
 										<p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor
 										tempus feugiat dolor lacinia cubilia curae integer congue leo metus
 										</p>
+										</div>
 																																							
 										</div>
 </td><td style="">
@@ -140,12 +142,14 @@
 										<img class="img-fluid" src="images/Ibnu_Affan-photo.png" alt="doctor-foto">	
 											
 										<!-- Title -->
+										<div style="margin-right: 20px; margin-left: 20px;">
 										<h5 class="h5-xs steelblue-color">Pediatrics</h5>
 
 										<!-- Text -->
 										<p>Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor
 										tempus feugiat dolor lacinia cubilia curae integer congue leo metus
 										</p>
+										</div>
 																																							
 										</div>
 
