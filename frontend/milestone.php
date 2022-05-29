a714cf1c466fb4216fbbd1f36b3b5df88f0ca6bd
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 04:10:44 +0700

    s

--- frontend/milestone.php
+++ frontend/milestone.php
@@ -257,7 +257,7 @@ body {
 										<div class="sbox-3 icon-sm" style="padding-top: 0; padding-left: 0; padding-right: 0;">
 
 										<!-- Icon -->
-										<img class="img-fluid" src="images/Ibnu_Affan-photo.png" alt="doctor-foto">	
+										<img class="img-fluid" src="images/head/legal.php" alt="doctor-foto">	
 											
 										<!-- Title -->
 										<div class="text-left" style="margin-right: 20px; margin-left: 20px;">
@@ -267,8 +267,7 @@ body {
 										<p>Chief Executive Officer
 										</p>
 										</div>
-										<br><a href="https://mtg.sticare.co.id/frontend/company-profile.php" class="btn btn-sm btn-orange">READ MORE</a>																										
-										</div>
+							</div>
     </li>
     <li>
       <div>
