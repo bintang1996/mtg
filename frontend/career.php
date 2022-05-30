86b233bad0561bd9d8a8b1bc5c0427b861ec7a14
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 09:56:06 +0700

    s

--- frontend/career.php
+++ frontend/career.php
@@ -23,7 +23,7 @@
 					</div>  <!-- End row -->	
 				</div>	<!-- End container -->		
 			</div>	<!-- END BREADCRUMB -->	
-        <div class="wide-100" style="background-color:#FFFFFF;">
+        <div class="wide-100" style="background-color:#FFFFFF; padding-bottom: 50px;">
             <div class="container" style="background-color:#FFFFFF;">
                 <div class="row">
                     <div class="col-lg-12">
