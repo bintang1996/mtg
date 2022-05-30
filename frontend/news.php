38a15f421c8e7e981f12789365ca4c61a7374858
bintang1996 <fzlr.bintang@gmail.com>
2022-05-31 00:27:07 +0700

    sdsd

--- frontend/news.php
+++ frontend/news.php
@@ -88,7 +88,7 @@ display: none;
 						<div class="col-lg-10 offset-lg-1 section-title">		
 
 						<div class="row">	
-						<div class="text-center" style="margin-bottom:30px;">		
+						<div class="col-lg-10 offset-lg-1 section-title" style="margin-bottom:30px;">		
 							<!-- Title 	-->	
 							<h3 class="h4-md steelblue-color text-center" style="color: #214068; PADDING-LEFT: 15PX;"><b><b>LATEST POST</b></b></h3>								
 						</div> 
