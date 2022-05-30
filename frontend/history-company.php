f644e7d47870d6bd3967c5fba2fc4d6344472488
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 21:06:24 +0700

    s

--- frontend/history-company.php
+++ frontend/history-company.php
@@ -1,13 +1,13 @@
 <TABLE width="100%"><tr><td width="19%">
 										<!-- SERVICE BOX #1 -->
-										<div class="sbox-3 icon-sm orange-hover" >
+										<div class="sbox-3 icon-sm orange-hover" style="padding: 10px;" >
 
 										<!-- Icon -->
 			
 											
 										<!-- Title -->
 										<div class="text-left">
-										<h5>AKTE</h5>
+										<h5 style="margin: 5px;">AKTE</h5>
 
 										<!-- Text -->
 										NO. 35,<BR>23 November 2012<br><br><br><br><br>
@@ -19,14 +19,14 @@
 
 
 										<!-- SERVICE BOX #1 -->
-										<div class="sbox-3 icon-sm orange-hover">
+										<div class="sbox-3 icon-sm orange-hover" style="padding: 10px;">
 
                     <!-- Icon -->
 
                       
                     <!-- Title -->
                     <div class="text-left">
-                    <h5>TDP</h5>
+                    <h5 style="margin: 5px;">TDP</h5>
 
                     <!-- Text -->
                     13.01.1.51.26643<br><br><br><br><br><br>
@@ -38,14 +38,14 @@
 
 
 										<!-- SERVICE BOX #1 -->
-										<div class="sbox-3 icon-sm orange-hover">
+										<div class="sbox-3 icon-sm orange-hover" style="padding: 10px;">
 
 										<!-- Icon -->
 									
 											
 										<!-- Title -->
 										<div class="text-left">
-										<h5>SIUP</h5>
+										<h5 style="margin: 5px;">SIUP</h5>
 
 										<!-- Text -->
 									503/482.A/436.6.11/2013<br><br><br><br><br><br>
@@ -57,14 +57,14 @@
 </td><td width="19%">
 
 										<!-- SERVICE BOX #1 -->
-										<div class="sbox-3 icon-sm orange-hover">
+										<div class="sbox-3 icon-sm orange-hover" style="padding: 10px;">
 
                     <!-- Icon -->
 
                       
                     <!-- Title -->
                     <div class="text-left">
-                    <h5>SIJUK</h5>
+                    <h5 style="margin: 5px;">SIJUK</h5>
 
                     <!-- Text -->
                    188.4/12/436.7.5/2019<br>- Bangunan Kesehatan/Rumah Sakit Pengolahan Limbah<br><br>
@@ -76,14 +76,14 @@
 
 </td><td width="20%">
 										<!-- SERVICE BOX #1 -->
-										<div class="sbox-3 icon-sm orange-hover">
+										<div class="sbox-3 icon-sm orange-hover" style="padding: 10px;">
 
 										<!-- Icon -->
 								
 											
 										<!-- Title -->
 										<div class="text-left">
-										<h5>IPAK</h5>
+										<h5 style="margin: 5px;">IPAK</h5>
 
 										<!-- Text -->
 								Elektromedik Radiasi, Elektromedik Non Radiasi, Non Elektronik, Non Steril, Non Elektromedik Steril & Diagnostik In Vitro
