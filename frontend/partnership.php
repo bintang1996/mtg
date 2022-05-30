9ad7af92d4adc6a19c115a1a06e4b2acd51c834b
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 20:04:03 +0700

    h

--- frontend/partnership.php
+++ frontend/partnership.php
@@ -105,67 +105,47 @@ display: none;
 					<div class="bottom-row" >
 						<div class="row d-flex align-items-center" >
 
+	<!-- Title -->
+	<h3 class="h3-md steelblue-color">Model Bisnis</h3>
 
 							<!-- INFO TEXT -->	
 							<div class="col-lg-6">
-								<div class="txt-block pc-30 wow fadeInUp" data-wow-delay="0.4s">
 
-									<!-- Title -->
-									<h3 class="h3-md steelblue-color">Model Bisnis</h3>
+							<table>
+								<tr>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+									<td></td>
+								</tr>
+							</table>
+							</div>
+							
+								<!-- INFO TEXT -->	
+								<div class="col-lg-6">
 
-									<!-- Text -->
-									<p class="mb-30">
-									</p>
-
-									<!-- Options List -->
-									<div class="row">
-
-										<div class="col-xl-6">
-
-											<!-- Option #1 -->
-											<div class="box-list">							
-												<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
-												<p class="p-sm">Kolaborasi BPJS, Funneling, PPP Model, Subdistribution</p>							
-											</div>
-
-											<!-- Option #2 -->
-											<div class="box-list">	
-												<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
-												<p class="p-sm">Dedicated Sales and Apps Person<br>Dedicated Sales and Apps person will give high chance to win</p>				
-											</div>
-
-											<!-- Option #3 -->
-											<div class="box-list">	
-												<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
-												<p class="p-sm">Bundle With Modality<br>Offer Bundle package with cathlab</p>				
-											</div>
-
-										</div>
-
-										<div class="col-xl-6">
-
-											<!-- Option #4 -->
-											<div class="box-list">	
-												<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
-												<p class="p-sm">E-catalog listed product<br>Register QFR in Government Ecommerce</p>				
-											</div>
-
-											<!-- Option #5 -->
-											<div class="box-list">							
-												<div class="box-list-icon blue-color"><i class="fas fa-angle-double-right"></i></div>
-												<p class="p-sm">On-site Support<br>Build confidence on Users/cardiologist</p>							
-											</div>
-										</div>
-									</div>	<!-- End Options List -->
+								<table>
+									<tr>
+										<td></td>
+										<td></td>
+									</tr>
+									<tr>
+									<td></td>
+									<td></td>
+								</tr>
+								<tr>
+									<td></td>
+									<td></td>
+								</tr>
+								</table>
 								</div>
-							</div>	<!-- END INFO TEXT -->	
-
-							<!-- INFO IMAGE -->
-							<div class="col-lg-6">
-								<div class="info-4-img text-center wow fadeInUp" data-wow-delay="0.6s">
-									<img style="border-radius: 10%;" width="100%" class="img-fluid" src="images/emergency_help_700x800.jpg" alt="info-image">
-								</div>
-							</div>						
+												
 						</div>    <!-- End row -->
 					</div>	<!-- END BOTTOM ROW -->
 				</div>	   <!-- End container -->
