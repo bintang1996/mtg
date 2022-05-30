3faa53e8dcc641398cc0ea8a77f4a1e11e343211
bintang1996 <fzlr.bintang@gmail.com>
2022-05-31 00:58:37 +0700

    s

--- frontend/our-products.php
+++ frontend/our-products.php
@@ -1127,7 +1127,7 @@ display: none;
 							?> 
 
 						<!-- DOCTOR #1 -->
-						<div class="col-md-6 col-lg-3">
+						<div class="col-md-6 col-lg-4">
 							<div class="doctor-1" style="border: 1px solid #4d4d4d; border-radius: 16px;">								
 														
 								<!-- Doctor Photo -->
