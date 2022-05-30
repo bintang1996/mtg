4962a0384b524b111ef224cef9b47c546bed2d0f
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 23:22:57 +0700

    s

--- frontend/konstruksi.php
+++ frontend/konstruksi.php
@@ -217,16 +217,16 @@ display: none;
 								<!-- Text -->
 								<p><table>
 								<tr>
-								<td valign="top">&#8226; </td> <td> Hepa Filter</td>
+								<td valign="top"><H6 STYLE="COLOR: #e86845">&#8226;</H6> </td> <td> &nbsp;&nbsp;Hepa Filter</td>
 								</tr>
 								<tr>
-								<td valign="top">&#8226; </td> <td>Instrument Ruang Operasi: Meja operasi, pass box, film viewer, display information, pendant, vynil lantai, dll</td>
+								<td valign="top">&#8226; </td> <td> &nbsp;&nbsp;Instrument Ruang Operasi: Meja operasi, pass box, film viewer, display information, pendant, vynil lantai, dll</td>
 								</tr>
 								<tr>
-								<td valign="top">&#8226; </td> <td>Finishing dinding ruang operasi dengan material lokal maupun import</td>
+								<td valign="top">&#8226; </td> <td> &nbsp;&nbsp;Finishing dinding ruang operasi dengan material lokal maupun import</td>
 								</tr>
 								<tr>
-								<td valign="top">&#8226; </td> <td>pintu hermatic dnegan mesin China, Jepang, maupun Eropa</td>
+								<td valign="top">&#8226; </td> <td> &nbsp;&nbsp;pintu hermatic dnegan mesin China, Jepang, maupun Eropa</td>
 								</tr>
 </table></p>
 							</div>
