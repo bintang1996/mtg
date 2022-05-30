6da8c3b02307d46fe148356a5e627ca9dd84e9e8
bintang1996 <fzlr.bintang@gmail.com>
2022-05-30 20:57:46 +0700

    a

--- frontend/history-company.php
+++ frontend/history-company.php
@@ -7,11 +7,11 @@
 											
 										<!-- Title -->
 										<div class="text-left">
-										<h5 class="h5-xs steelblue-color h5">AKTE</h5>
+										<b>AKTE</b><br>
 
 										<!-- Text -->
-										<p class="p5">NO. 35,<BR>23 November 2012
-										</p>
+										NO. 35,<BR>23 November 2012
+										
 										</div>
 								</div>
 </td><td width="19%">
@@ -26,11 +26,11 @@
                       
                     <!-- Title -->
                     <div class="text-left">
-                    <h5 class="h5 h5-xs steelblue-color">TDP</h5>
+                    <b>TDP</b><br>
 
                     <!-- Text -->
-                    <p class="p5">13.01.1.51.26643
-                    </p>
+                    13.01.1.51.26643
+                   
                     </div>
                   </div>
 
@@ -45,11 +45,11 @@
 											
 										<!-- Title -->
 										<div class="text-left">
-										<h5 class="h5 h5-xs steelblue-color">SIUP</h5>
+										<b>SIUP</b><br>
 
 										<!-- Text -->
-										<p class="p5">503/482.A/436.6.11/2013
-										</p>
+									503/482.A/436.6.11/2013
+										
 										</div>
 										</div>
 
@@ -64,11 +64,11 @@
                       
                     <!-- Title -->
                     <div class="text-left">
-                    <h5 class="h5 h5-xs steelblue-color">SIJUK</h5>
+                    <b>SIJUK</b>
 
                     <!-- Text -->
-                    <p class="p5">188.4/12/436.7.5/2019<br>- Bangunan Kesehatan/Rumah Sakit Pengolahan Limbah
-                    </p>
+                   188.4/12/436.7.5/2019<br>- Bangunan Kesehatan/Rumah Sakit Pengolahan Limbah
+                    
                     </div>
                    </div>
 
@@ -83,11 +83,11 @@
 											
 										<!-- Title -->
 										<div class="text-left">
-										<h5 class="h5 h5-xs steelblue-color">IPAK</h5>
+										<b>IPAK</b>
 
 										<!-- Text -->
-										<p class="p5">Elektromedik Radiasi, Elektromedik Non Radiasi, Non Elektronik, Non Steril, Non Elektromedik Steril & Diagnostik In Vitro
-										</p>
+								Elektromedik Radiasi, Elektromedik Non Radiasi, Non Elektronik, Non Steril, Non Elektromedik Steril & Diagnostik In Vitro
+								
 										</div>
 								    </div>
                    
