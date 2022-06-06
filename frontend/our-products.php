b47dbb9c4a262d84413851ba1be76df66e3c5a30
bintang1996 <fzlr.bintang@gmail.com>
2022-06-06 21:25:20 +0700

    a

--- frontend/our-products.php
+++ frontend/our-products.php
@@ -1189,7 +1189,7 @@ display: none;
 
 									<p class="p-sm grey-color"><?php $rt=$d1['p_desc']; echo substr("$rt", 0, 100); ?>
 									</p>
-									<br><a href="product-details.php?id=<?php echo $x;?>" >Read More<a>
+									
 								</div>	
 
 							</div>								
