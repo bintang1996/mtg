0f84fbd4262faaf5e958bb26a8bc539fba5378a4
bintang1996 <fzlr.bintang@gmail.com>
2022-06-06 14:08:04 +0700

    s

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -152,8 +152,11 @@ display: none;
 										<br><a href="https://mtg.sticare.co.id/frontend/company-profile.php" class="btn btn-sm btn-orange">READ MORE</a>																										
 										</div>
 	
-</td><td>
-	<td style="width: 2%;"></td>
+</td>
+<td style="width: 2%;"></td>
+
+<td>
+	
 										<div class="sbox-3 icon-sm" style="padding-top: 0; padding-left: 0; padding-right: 0;">
 
 										<!-- Icon -->
@@ -170,8 +173,11 @@ display: none;
 										<br><a href="https://mtg.sticare.co.id/frontend/company-profile.php" class="btn btn-sm btn-orange">READ MORE</a>
 																																							
 										</div>
-</td><td>
+</td>
 <td style="width: 2%;"></td>
+
+<td>
+
 										<div class="sbox-3 icon-sm" style="padding-top: 0; padding-left: 0; padding-right: 0;">
 
 										<!-- Icon -->
