ace40325243181c33a922f527a87075e8a52fe4d
bintang1996 <fzlr.bintang@gmail.com>
2022-06-12 10:39:17 +0700

    fdfd

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -601,6 +601,14 @@ display: none;
   					</div>
 
 					  <div class="panel-body" style="text-align: center; overflow: hidden; ">
+    				<img style="max-height: 50px; margin-top:20px; margin-bottom:20px; margin-right:20px; margin-left:0;" src="images/networks/br-5.png">
+  					</div>
+
+					  <div class="panel-body" style="text-align: center; overflow: hidden; ">
+    				<img style="max-height: 50px; margin-top:20px; margin-bottom:20px; margin-right:20px; margin-left:0;" src="images/networks/br-6.png">
+  					</div>
+
+					  <div class="panel-body" style="text-align: center; overflow: hidden; ">
     				<img style="max-height: 30px; margin:20px;" src="images/networks/Picture28.png">
   					</div>
 
