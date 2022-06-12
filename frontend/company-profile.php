422aec7866a75ef53fc41b61920e59b8db6b5aec
bintang1996 <fzlr.bintang@gmail.com>
2022-06-12 10:40:55 +0700

    s

--- frontend/company-profile.php
+++ frontend/company-profile.php
@@ -472,10 +472,11 @@ display: none;
 					<div class="panel-body" style="text-align: center; overflow: hidden; ">
     				<img style="max-height: 50px; margin-top:20px; margin-bottom:20px; margin-right:20px; margin-left:0;" src="images/networks/Picture1.png">
   					</div>
-
-					<div class="panel-body" style="text-align: center; overflow: hidden; ">
-    				<img  style="max-height: 50px; margin-top:20px; margin-bottom:20px; margin-right:20px; margin-left:0;" src="images/networks/Picture2.png">
+					  <div class="panel-body" style="text-align: center; overflow: hidden; ">
+    				<img style="max-height: 30px; margin:20px;" src="images/networks/Picture28.png">
   					</div>
+
+					
 
 
 					<div class="panel-body" style="text-align: center; overflow: hidden;">
@@ -608,10 +609,10 @@ display: none;
     				<img style="max-height: 50px; margin-top:20px; margin-bottom:20px; margin-right:20px; margin-left:0;" src="images/networks/br-6.png">
   					</div>
 
+					
 					  <div class="panel-body" style="text-align: center; overflow: hidden; ">
-    				<img style="max-height: 30px; margin:20px;" src="images/networks/Picture28.png">
+    				<img  style="max-height: 50px; margin-top:20px; margin-bottom:20px; margin-right:20px; margin-left:0;" src="images/networks/Picture2.png">
   					</div>
-
 					
 
 					</div>	    <!-- End row2 -->
