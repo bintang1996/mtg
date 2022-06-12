899c815f7b4d77482361c0bc70834723af5332bb
bintang1996 <fzlr.bintang@gmail.com>
2022-06-12 12:45:47 +0700

    s

--- frontend/our-products.php
+++ frontend/our-products.php
@@ -63,7 +63,7 @@ display: none;
 			</section>	<!-- END HERO-2 -->
 
 <style>
-	.accordion{
+	#accordion{
 		-webkit-box-shadow: -1px -1px 1px 1px rgba(0,0,0,0.5);
   -moz-box-shadow: -1px -1px 1px 1px rgba(0,0,0,0.425);
   box-shadow: -1px -1px 1px 1px rgba(0,0,0,0.35);
