5b1fc3193a6e9127c55dd0f4feef80334bbbc3e9
bintang1996 <fzlr.bintang@gmail.com>
2022-06-12 12:28:57 +0700

    s

--- frontend/our-products.php
+++ frontend/our-products.php
@@ -64,7 +64,12 @@ display: none;
 
 <style>
 	.card-header{
-		border:1px solid #4d4d4d; background-color: #fff; color: #fff;
+		.
+  /* offset-x | offset-y | blur-radius | spread-radius | color */
+  -webkit-box-shadow: -2px -1px 15px 7px rgba(0,0,0,0.5);
+  -moz-box-shadow: -3px -2px 30px 14px rgba(0,0,0,0.425);
+  box-shadow: -4px -3px 45px 21px rgba(0,0,0,0.35);
+
 	}
 </style>
 
