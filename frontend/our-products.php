52a745b431e9747a04783fee54846ca08b38b22b
bintang1996 <fzlr.bintang@gmail.com>
2022-06-12 12:23:14 +0700

    p

--- frontend/our-products.php
+++ frontend/our-products.php
@@ -955,7 +955,7 @@ display: none;
 									</td>
 									<td style="text-align: right; align:right;">
 									<?php
-										$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM category where cat_name='SAV'") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
+										$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM category where cat_name='Agfa'") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
 										while($d=mysqli_fetch_array($sql)){
 										?> 
 										
@@ -976,7 +976,7 @@ display: none;
 				<div class="row">
 				<?php
 				$r=$_GET['id'];
-				$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM collections where cat_name='AGFA'") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
+				$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM collections where cat_name='Agfa'") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
 				while($d=mysqli_fetch_array($sql)){ $x=$d['p_id'];
 				$sql1 = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM product where id='$x'") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
 				while($d1=mysqli_fetch_array($sql1)){ 
