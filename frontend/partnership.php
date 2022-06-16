17537606550d5f95ca505fa9bcd028c250f72fe7
bintang1996 <fzlr.bintang@gmail.com>
2022-06-16 16:40:58 +0700

    klk'

--- frontend/partnership.php
+++ frontend/partnership.php
@@ -254,7 +254,7 @@ display: none;
 												
 
 												<!-- Button -->
-												<a href="service-1.html" class="btn btn-blue orange-hover mt-30">Hubungi Kami</a>
+												<a href="https://wa.me/+6281280001102" class="btn btn-blue orange-hover mt-30">Hubungi Kami</a>
 
 											</div>	
 										</div>	<!-- END TAB-1 TEXT -->
@@ -291,7 +291,7 @@ display: none;
 
 
 												<!-- Button -->
-												<a href="service-2.html" class="btn btn-blue orange-hover mt-30">Hubungi Kami</a>
+												<a href="https://wa.me/+6281280001102" class="btn btn-blue orange-hover mt-30">Hubungi Kami</a>
 
 											</div>	
 										</div>	<!-- END TAB-2 TEXT -->
@@ -379,7 +379,7 @@ display: none;
 												</div>	<!-- End Options List -->
 
 												<!-- Button -->
-												<a href="service-1.html" class="btn btn-blue orange-hover mt-30">Lebih Lanjut</a>
+												<a href="https://wa.me/+6281280001102" class="btn btn-blue orange-hover mt-30">Lebih Lanjut</a>
 
 											</div>	
 										</div>	<!-- END TAB-3 TEXT -->
@@ -467,7 +467,7 @@ display: none;
 												</div>	<!-- End Options List -->
 
 												<!-- Button -->
-												<a href="service-2.html" class="btn btn-blue orange-hover mt-30">Hubungi Kami</a>
+												<a href="https://wa.me/+6281280001102" class="btn btn-blue orange-hover mt-30">Hubungi Kami</a>
 
 											</div>	
 										</div>	<!-- END TAB-4 TEXT -->
