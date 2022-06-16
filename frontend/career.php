acdb7f4dc0043fbd3412fb82f85b59956fe959ed
bintang1996 <fzlr.bintang@gmail.com>
2022-06-16 20:08:06 +0700

    lkl

--- frontend/career.php
+++ frontend/career.php
@@ -94,6 +94,8 @@ display: none;
 							<!-- Text -->
 							<p>Berikut Lowongan Pekerjaan di Meditrans. 
 							</p>
+
+							<p><i>Tidak ada lowongan pekerjaan saat ini</i></p>
 								
 						</div>
 			</div>
