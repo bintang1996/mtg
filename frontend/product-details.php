6f4f2cbda91fc139dede8ffd9df26488994fa00a
bintang1996 <fzlr.bintang@gmail.com>
2022-06-18 21:35:53 +0700

    fida-change banner detail produk

--- frontend/product-details.php
+++ frontend/product-details.php
@@ -1,28 +1,57 @@
 <?php include "header.php";?>
-<!-- BREADCRUMB
-			============================================= -->
-			<div id="breadcrumb" class="division" >
-				<div class="container">
-					<div class="row">						
-						<div class="col">
-							<div class=" breadcrumb-holder">
 
-								<!-- Breadcrumb Nav -->
-								<nav aria-label="breadcrumb">
-								  	<ol class="breadcrumb">
-								    	<li class="breadcrumb-item"><a href="index.html">Home</a></li>
-								    	<li class="breadcrumb-item active" aria-current="page">Product Details</li>
-								  	</ol>
-								</nav>
+<!-- HERO-2
+			============================================= -->	
+			<section id="hero-2" class="hero-section division">
 
-								<!-- Title -->
-								<h4 class="h4-sm steelblue-color">Product Details</h4>
 
-							</div>
-						</div>
-					</div>  <!-- End row -->	
-				</div>	<!-- End container -->		
-			</div>	<!-- END BREADCRUMB -->	
+				<!-- SLIDER -->
+				<div class="slider" style="height: 500px;">
+			    	<ul class="slides">
+
+
+				     	<!-- SLIDE #1 -->
+				      	
+
+    	
+                                <li style="height: 500px;">			
+								<img class="img-fluid" width="100%" height="500px" src="images/detailproduk.jpg" alt="hero-image">
+                                
+							<!-- Image Caption -->
+							<div class="caption d-flex align-items-center left-align" style="background: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.2)); ">
+		       					<div class="container">
+		       						<div class="row" >
+		       							<div class="col-md-12 col-lg-10" >
+		       								<div class="caption-txt">
+						       					<!-- Title -->
+						       					<h3 class="white-color judul1">DETAIL PRODUK</h3>
+
+						       					<!-- Option Box #1 -->
+												<div class="box-list">							
+													<div class="box-list-icon white-color"></div>
+																			
+												</div>
+									
+									    
+											</div>
+										</div>
+									</div>  <!-- End row -->
+								</div>  <!-- End container -->
+					        </div>	<!-- End Image Caption -->
+
+							
+							
+							</li>
+                       
+						
+					    	<!-- END SLIDE #1 -->
+
+
+				    </ul>
+			  	</div>	<!-- END SLIDER -->
+
+
+			</section>	<!-- END HERO-2 -->
 
 
 			<!-- INFO-4
