6590a3e2edb3d224fe3caffdb364965c9f76ecb6
bintang1996 <fzlr.bintang@gmail.com>
2022-06-23 18:42:35 +0700

    fida-from product brand to brand

--- frontend/index.php
+++ frontend/index.php
@@ -398,7 +398,7 @@
 					<div class="row">	
 						<div class="col-lg-10 offset-lg-1 section-title" style="margin-bottom:30px;">		
 							<!-- Title 	-->	
-							<h3 class="h4-md steelblue-color" style="color: #214068;"><b><b>PRODUCT BRAND</b></b></h3>								
+							<h3 class="h4-md steelblue-color" style="color: #214068;"><b><b>BRAND</b></b></h3>								
 						</div> 
 					</div>
 					<div class="row d-flex align-items-center text-center">
