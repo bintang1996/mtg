9d3069781c51295f462495c3235cbc5f8c98c8f0
bintang1996 <fzlr.bintang@gmail.com>
2022-06-23 19:54:24 +0700

    fida-changes on contact us

--- frontend/contact-us.php
+++ frontend/contact-us.php
@@ -81,28 +81,28 @@ display: none;
 
 		 					<!-- General Information -->
 		 					<div class="contact-box mb-40">
-								<h5 class="h5-sm steelblue-color">Jakarta Head Office</h5>
+								<h5 class="h5-sm steelblue-color">Kantor Pusat Jakarta</h5>
 								<p>Jalan Munggang no 86-87</p> 
 								<p>Balaikambang, Keramat Jati, Jakarta Timur</p>
                                 <p>13530</p>
-								<p>Phone: +62 21 2204 7248</p>
+								<p>Telepon: +62 21 2204 7248</p>
 								<p>Email: <a href="mailto:info@meditrans.id" class="blue-color">info@meditrans.id</a></p>
 		 					</div>
 
 		 					<!-- Patient Experience -->
 		 					<div class="contact-box mb-40">
-								<h5 class="h5-sm steelblue-color">Surabaya Head Office</h5>
+								<h5 class="h5-sm steelblue-color">Kantor Pusat Surabaya</h5>
 								<p>Rungkut Menanggal Harapan O/28</p>
                                 <p>Rungkut, Surabaya</p>
                                 <p>60239</p>
-								<p>Phone: +62 21 2287 9398</p>
+								<p>Telepon: +62 21 2287 9398</p>
 								<p>Email: <a href="mailto:info@meditrans.id" class="blue-color">info@meditrans.id</a></p>
 		 					</div>
 
 		 					<!-- Working Hours -->
 		 					<div class="contact-box mb-40">
-								<h5 class="h5-sm steelblue-color">Working Hours</h5>
-								<p>Monday – Friday : 8:00 - 17:00 WIB</p> 
+								<h5 class="h5-sm steelblue-color">Jam Kerja</h5>
+								<p>Senin – Jumat : 8:00 - 17:00 WIB</p> 
 								
 		 					</div>
 
