bd28c19cd2a77673f9acbfd73d659428ccd7c858
bintang1996 <fzlr.bintang@gmail.com>
2022-06-23 19:22:02 +0700

    j

--- frontend/header.php
+++ frontend/header.php
@@ -177,7 +177,7 @@ h6
     					<!-- MAIN MENU -->
       					<nav class="wsmenu clearfix">
         					<ul class="wsmenu-list">
-							<li class="nl-simple" aria-haspopup="true"><a href="index.php" style="padding-left:0px; padding-right:18px;">BERANDA</a></li>
+							<li class="nl-simple" aria-haspopup="true"><a href="index.php" style="padding-left:12px; padding-right:18px;">BERANDA</a></li>
 							
 							<?php
 									$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM navbar where cat_desc=1 and cat_name!='BERITA' and cat_name!='KARIR' and cat_name!='KONTAK' and cat_name!='HOME'");
@@ -185,7 +185,7 @@ h6
 										?>  	
 
 							<!-- DROPDOWN MENU -->
-							<li aria-haspopup="true" ><a style="padding-left:0px; padding-right:18px;" href="<?php echo $d['logo']; ?>"><?php echo $d['cat_name'];?><span class="wsarrow"></span></a>
+							<li aria-haspopup="true" ><a style="padding-left:12px; padding-right:18px;" href="<?php echo $d['logo']; ?>"><?php echo $d['cat_name'];?><span class="wsarrow"></span></a>
 					           		<ul class="sub-menu">
 									   <?php
 										$sql2 = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM navbar where cat_parent='$x' and cat_desc=2");
@@ -202,13 +202,13 @@ h6
 									while($d=mysqli_fetch_array($sql)){
 										?>  
 								<!-- SIMPLE NAVIGATION LINK -->
-								<li class="nl-simple" aria-haspopup="true"><a style="padding-left:0px; padding-right:18px;" href="<?php echo $d['logo']; ?>"><?php echo $d['cat_name'] ?></a></li>
+								<li class="nl-simple" aria-haspopup="true"><a style="padding-left:12px; padding-right:18px;" href="<?php echo $d['logo']; ?>"><?php echo $d['cat_name'] ?></a></li>
 								<?php } ?>	
 
 					          	 
 								
         						<!-- DROPDOWN MENU -->
-					        	<li aria-haspopup="true" ><a style="padding-left:0px; padding-right:18px;" href="#"><img src="images/flag/1.png" width="20px"><span class="wsarrow"></span></a>
+					        	<li aria-haspopup="true" ><a style="padding-left:12px; padding-right:18px;" href="#"><img src="images/flag/1.png" width="20px"><span class="wsarrow"></span></a>
 					           		<ul class="sub-menu">
 					           			<li><a href="#"><img src="images/flag/1.png" width="20px">&nbsp;&nbsp;INDONESIA</a></li>
 					              		<li><a href="#"><img src="images/flag/2.png" width="20px">&nbsp;&nbsp;ENGLISH</a></li>
