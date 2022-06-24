1c24a85a9f7ce1b1c0e3a0370925e581e28c435b
bintang1996 <fzlr.bintang@gmail.com>
2022-06-24 10:49:43 +0700

    s

--- frontend/history-company.php
+++ frontend/history-company.php
@@ -1,7 +1,13 @@
+<style>
+	.sbox-3{
+		min-height: 170px;
+		max-height: 170px;
+	}
+	</style>
 <div style="display:inline-block;width:100%;overflow-y: hidden;">
 <TABLE width="100%"><tr><td width="19%">
 										<!-- SERVICE BOX #1 -->
-										<div class="sbox-3 icon-sm orange-hover" width="19%" style="padding: 15px;" >
+										<div class="sbox-3 icon-sm orange-hover"  style="padding: 15px;" >
 
 										<!-- Icon -->
 			
