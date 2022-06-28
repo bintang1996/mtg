f3f2cee24febf7e3593854d9526aadf682a8c011
bintang1996 <fzlr.bintang@gmail.com>
2022-06-28 09:08:40 +0700

    s

--- frontend/index.php
+++ frontend/index.php
@@ -556,7 +556,7 @@
 
 							<?php
                     $r=$_GET['id'];
-							$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM collections_a where cat_name = 'NEWS' limit 9") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
+							$sql = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM collections_a where cat_name = 'NEWS' OR cat_name = 'EDUCATION' OR cat_name = 'OUR STORIES' limit 9") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
 							while($d=mysqli_fetch_array($sql)){ $x=$d['p_id'];
                                 $sql1 = mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM artikel where id='$x' order by id desc") or die ("Query gagal dengan error: ".mysqli_error($GLOBALS["___mysqli_ston"]));
                                 while($d1=mysqli_fetch_array($sql1)){ 
@@ -590,7 +590,7 @@
 										</div>	
 
 										<!-- Testimonial Text -->
-										<p style="font-size: 0.5em; margin:0px;"><?php $rt=$d1['p_desc']; echo substr("$rt", 0, 100); ?>... <a style="font-size: 1em; color: #e86845;" href="article-details.php?id=<?php echo $d1['id']; ?>">Lebih lanjut.</a></p>
+										<p style="font-size: 0.5em; margin:0px;"><?php $rt=$d1['p_desc']; echo substr("$rt", 0, 100); ?>... <a style="font-size: 1em; color: #e86845;" href="article-details.php?id=<?php echo $d1['id']; ?>">Read more.</a></p>
 														
 																
 									</div>						
